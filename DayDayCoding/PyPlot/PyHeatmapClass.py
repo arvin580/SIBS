1119742dc656987f154e076f71c5f772e59e1cbc
hanice <sunahnice@gmail.com>
2012-09-10 18:16:50 +0800

    9.20

---
+++ DayDayCoding/PyPlot/PyHeatmapClass.py
@@ -0,0 +1,141 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import scipy.cluster.hierarchy as sch
+import time
+
+
+class PyHeatmap():
+    def __init__(self):
+        self.data = []
+        self.rowname = []
+        self.colname = []
+
+    def readFile(self, inF, header=True):
+        if inF.find('.csv') != -1:
+            sep=','
+        else:
+            sep='\t'
+        data = []
+        rowname = []
+        colname = []
+        if header:
+            inFile = open(inF)
+            row = 0
+            for line in inFile:
+                line = line.strip()
+                fields = line.split(sep)
+                row += 1
+                if row == 1:
+                    colname = fields[1:]
+                else:
+                    data.append([float(x) for x in fields[1:]])
+                    rowname.append(fields[0])
+            inFile.close()
+        else:
+            inFile = open(inF)
+            for line in inFile:
+                line = line.strip()
+                fields = line.split('\t')
+                data.append([float(x) for x in fields])
+            inFile.close()
+            rowname = ['g' + str(x) for x in range(1,len(data)+1)]
+            colname = ['s' + str(x) for x in range(1,len(data[0])+1)]
+        self.data = np.array(data)
+        self.rowname=np.array(rowname)
+        self.colname=np.array(colname)
+    
+    def readData(self,aList):
+        rowname = ['g' + str(x) for x in range(1,len(aList)+1)]
+        colname = ['s' + str(x) for x in range(1,len(aList[0])+1)]
+        self.data=np.array(aList)
+        self.rowname=np.array(rowname)
+        self.colname=np.array(colname)
+
+    def printData(self):
+        print('data:')
+        for x in self.data:
+            for y in x:
+                print(str(y) + '\t'),
+            print('\n'),
+        print('row name:')
+        print('\t'.join(self.rowname))
+        print('column name:')
+        print('\t'.join(self.colname))
+    
+    def stadand(self):
+        pass
+
+    def rowDendrogram(self, ax):
+        d = sch.distance.pdist(self.data)
+        rl = sch.linkage(d, method='complete')
+        rd = sch.dendrogram(rl, orientation='right')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        index = rd['leaves']
+        self.data = self.data[index,:]
+        print(index)
+        self.rowname = self.rowname[index[::-1]]
+
+
+    def colDendrogram(self, ax):
+        d = sch.distance.pdist(self.data.transpose())
+        cl = sch.linkage(d, method='complete')
+        cd = sch.dendrogram(cl, orientation='top')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        index = cd['leaves']
+        self.data = self.data[:,index]
+        self.colname = self.colname[index]
+
+    def showDataName(self, ax, color):
+        ax.matshow(self.data, cmap=color, aspect='auto', origin='lower')
+        ax.set_xticks(range(len(self.colname)))
+        ax.set_yticks(range(len(self.rowname)))
+        ax.yaxis.tick_right()
+        ax.xaxis.tick_bottom()
+        ax.set_xticklabels(self.colname)
+        ax.set_yticklabels(self.rowname)
+        print(self.rowname)
+    
+    def showDataColor(self, ax, color, fig):
+        axm = ax.matshow(self.data, cmap=color, aspect='auto', origin='lower')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        axcolor = fig.add_axes([0.91,0.1,0.02,0.7])
+        plt.colorbar(axm, cax=axcolor)
+        print(self.rowname)
+ 
+    def showDataNameColor(self, ax, color, fig):
+        #norm=matplotlib.colors.Normalize(-self.data.max()/2,self.data.max()/2)
+        colors = [('white')] + [(color(i)) for i in xrange(1,256)]
+        new_map = matplotlib.colors.LinearSegmentedColormap.from_list('new_map', colors, N=256)
+        axm = ax.matshow(self.data, cmap=new_map, aspect='auto', origin='lower')
+        ax.set_xticks(range(len(self.colname)))
+        ax.set_yticks(range(len(self.rowname)))
+        ax.yaxis.tick_right()
+        ax.xaxis.tick_bottom()
+        ax.set_xticklabels(self.colname)
+        ax.set_yticklabels(self.rowname)
+        axcolor = fig.add_axes([0.2,0.05,0.7,0.02])
+        plt.colorbar(axm, cax=axcolor, orientation='horizontal')
+        print(self.rowname)
+ 
+    def heatmap(self, color=plt.cm.jet):
+        fig = plt.figure()
+        ax = fig.add_axes([0,0.1,0.2,0.7])
+        self.rowDendrogram(ax)
+        ax = fig.add_axes([0.2,0.8,0.7,0.2])
+        self.colDendrogram(ax)
+        ax = fig.add_axes([0.2,0.1,0.7,0.7])
+        self.showDataNameColor(ax, color, fig)
+        plt.show()
+
+
+ph = PyHeatmap()
+ph.readFile('ppg2008.csv.txt')
+#ph.readFile('test')
+#ph.readData([[1,2,3,4,5],[5,6,7,8,1],[1,1,2,3,2]])
+ph.printData()
+ph.heatmap(plt.cm.gist_yarg)
--- DayDayCoding/PyPlot/PyPlotClass.py
+++ DayDayCoding/PyPlot/PyPlotClass.py
@@ -1,1 +1,646 @@
-../GenomeSeq/PyPlotClass.py
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt 
+import time
+
+class PyPlot :
+
+    def ___init__(self,filename='pyplot.'+str(time.strftime("%Y.%m.%d.%H.%M.%S",time.localtime()))+'.pdf') :
+        self.filename=filename
+        #self.colors=['r','g','b','y','c','m','w','k']
+    def __init__(self,filename='') :
+        if filename :
+            self.filename=filename
+        else :
+            self.filename='pyplot.'+str(time.strftime("%Y.%m.%d.%H.%M.%S",time.localtime()))+'.pdf'
+     
+    def get_color(self,i,N) :
+        #if i <len(self.colors) :
+        #    return self.colors[i]
+        #else :
+        #    return self.colors[-1]
+        cm=plt.get_cmap('gist_rainbow')
+        return(cm(i/float(N)))
+        
+
+    def show(self):
+        #plt.show()
+        plt.savefig(self.filename)
+    
+    def plot(self,*arg) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        if len(arg)==1 :
+            yList=arg[0]
+            ax.plot(yList)
+            self.show()
+        if len(arg)==2 :
+            xList=arg[0]
+            yList=arg[1]
+            ax.plot(xList,yList)
+            self.show()
+            
+    def single_bar(self,yList,xLabel=0,xTitle=0,yTitle=0) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        n=len(yList)
+        xList=np.arange(n)
+
+        width=0.7
+        ax.bar(xList,yList,width,color=self.get_color(1,1))
+        #ax.bar(xList,yList,width,color=matplotlib.colors.colorConverter.to_rgb('0.3'))
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,yList.max()*1.1)
+        ax.set_xticks(xList+width/2)
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+        self.show()
+
+
+    def single_error_bar(self,yList,errList,xLabel=0,xTitle=0,yTitle=0):
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        errList=np.array(errList)
+        n=len(yList)
+        xList=np.arange(n)
+
+        width=0.7
+        ax.bar(xList,yList,width,color=self.get_color(1,1),yerr=errList)
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,(yList+errList).max()*1.1)
+        ax.set_xticks(xList+width/2)
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+        self.show()
+
+    def multi_bar(self,yList,xLabel=0,xTitle=0,yTitle=0,legTitle=0,legX=0,legY=0) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        n=len(yList[0])
+        N=len(yList)
+
+        xList=np.arange(n)
+
+        width=0.9/N
+
+        bar=[]
+        for i in range(N) :
+            bar.append(ax.bar(xList+i*width,yList[i],width,color=self.get_color(i,N)))
+
+
+        ax.set_xlim(-0.1,n)
+        ax.set_ylim(0,yList.max()*1.1)
+        ax.set_xticks(xList+0.9/2)
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+
+        if legTitle==0 :
+            legTitle=['type'+str(i) for i in range(1,N+1)]
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=0.97
+        if xTitle :
+            ax.set_xlabel(xTitle)
+        if yTitle :
+            ax.set_ylabel(yTitle)
+
+
+        legBar=[bar[i][0] for i in range(N)]
+
+        ax.legend(legBar,legTitle,loc='upper right',bbox_to_anchor=[legX,legY])
+
+        self.show()
+
+    def multi_bar_star(self,yList,xLabel=0,xTitle=0,yTitle=0,legTitle=0,legX=0,legY=0,starList=[]) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        n=len(yList[0])
+        N=len(yList)
+
+        xList=np.arange(n)
+
+        width=0.9/N
+
+        bar=[]
+        for i in range(N) :
+            bar.append(ax.bar(xList+i*width,yList[i],width,color=self.get_color(i,N)))
+
+
+        ax.set_xlim(-0.1,n)
+        ax.set_ylim(0,yList.max()*1.1)
+        ax.set_xticks(xList+0.9/2)
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+
+        if legTitle==0 :
+            legTitle=['type'+str(i) for i in range(1,N+1)]
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=0.97
+        if xTitle :
+            ax.set_xlabel(xTitle)
+        if yTitle :
+            ax.set_ylabel(yTitle)
+
+        #ax.text(xList+0.9/2,[1000,1000,1000,1000,1000,1000],'***')
+        for i in range(len(starList)) :
+            if starList[i] :
+                ax.text((xList+0.9/2)[i],yList[:,i].max(),starList[i],horizontalalignment='center')
+
+        legBar=[bar[i][0] for i in range(N)]
+
+        ax.legend(legBar,legTitle,loc='upper right',bbox_to_anchor=[legX,legY])
+
+        self.show()
+
+    def multi_bar_vertical(self,yList,xLabel=0,xTitle=0,yTitle=0,legTitle=0,legX=0,legY=0) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        n=len(yList[0])
+        N=len(yList)
+
+        xList=np.arange(n)
+
+        width=0.7
+
+        bar=[]
+        bar.append(ax.bar(xList,yList[0],width,color=self.get_color(0,N)))
+        for i in range(1,N) :
+            bar.append(ax.bar(xList,yList[i],width,bottom=sum(yList[0:i]),color=self.get_color(i,N)))
+
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,sum(yList[0:]).max()*1.1)
+        ax.set_xticks(xList+width/2)
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+
+
+        if legTitle==0 :
+            legTitle=['type'+str(i) for i in range(1,N+1)]
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=0.97
+
+        legBar=[bar[i][0] for i in range(N)]
+
+        ax.legend(legBar,legTitle,loc='upper right',bbox_to_anchor=[legX,legY])
+
+        self.show()
+
+
+    def multi_bar_vertical_xlael_vertical(self,yList,xLabel=0,xTitle=0,yTitle=0,legTitle=0,legX=0,legY=0) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        n=len(yList[0])
+        N=len(yList)
+
+        xList=np.arange(n)
+
+        width=0.7
+
+        bar=[]
+        bar.append(ax.bar(xList,yList[0],width,color=self.get_color(0,N)))
+        for i in range(1,N) :
+            bar.append(ax.bar(xList,yList[i],width,bottom=sum(yList[0:i]),color=self.get_color(i,N)))
+
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,sum(yList[0:]).max()*1.1)
+        ax.set_xticks(xList+width/2)
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel,rotation='vertical')
+
+
+        if legTitle==0 :
+            legTitle=['type'+str(i) for i in range(1,N+1)]
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=0.97
+
+        legBar=[bar[i][0] for i in range(N)]
+
+        ax.legend(legBar,legTitle,loc='upper right',bbox_to_anchor=[legX,legY])
+
+        #self.show()
+        plt.savefig(self.filename,bbox_inches='tight')
+
+
+    def multi_bar_vertical_proportion(self,yList,xLabel=0,xTitle=0,yTitle=0,legTitle=0,legX=0,legY=0) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        n=len(yList[0])
+        N=len(yList)
+
+        xList=np.arange(n)
+
+        width=0.7
+
+        bar=[]
+        bar.append(ax.bar(xList,yList[0]/sum(yList[0:]).astype('float'),width,color=self.get_color(0,N)))
+        for i in range(1,N) :
+            bar.append(ax.bar(xList,yList[i],width,bottom=sum(yList[0:i])/sum(yList[0:]).astype('float'),color=self.get_color(i,N)))
+
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,1)
+
+        ax.set_xticks(xList+width/2)
+        ax.set_yticks([0.0,0.2,0.4,0.6,0.8,1.0])
+        ax.set_yticklabels(['0%','20%','40%','60%','80%','100%'])
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+
+
+
+        if legTitle==0 :
+            legTitle=['type'+str(i) for i in range(1,N+1)]
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=0.97
+
+        legBar=[bar[i][0] for i in range(N)]
+
+        ax.legend(legBar,legTitle,loc='upper right',bbox_to_anchor=[legX,legY])
+
+        self.show()
+
+    def single_bar_multi_bar_vertical_proportion(self,yList1,yList2,xLabel=0,xTitle=0,yTitle1=0,yTitle2=0,legTitle=0,legX=0,legY=0) :
+        print('haha')
+
+        bar2=[]
+        fig = plt.figure()
+
+        ax=fig.add_subplot(212)
+
+        yList2=np.array(yList2)
+        n=len(yList2[0])
+        N=len(yList2)
+
+        xList=np.arange(n)
+
+        width=0.7
+
+        bar2.append(ax.bar(xList,yList2[0]/sum(yList2[0:]).astype('float'),width,color=self.get_color(1,N+1)))
+        for i in range(1,N) :
+            bar2.append(ax.bar(xList,yList2[i]/sum(yList2[0:]).astype('float'),width,bottom=sum(yList2[0:i])/sum(yList2[0:]).astype('float'),color=self.get_color(i+1,N+1)))
+
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,1)
+
+        ax.set_xticks(xList+width/2)
+        ax.set_yticks([0.0,0.2,0.4,0.6,0.8,1.0])
+        ax.set_yticklabels(['0%','20%','40%','60%','80%','100%'])
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+        if yTitle2==0 :
+            yTitle2='SNV(%)'
+        ax.set_ylabel(yTitle2)
+
+
+
+        #####
+
+        ax=fig.add_subplot(211)
+        bar1=[]
+
+        yList1=np.array(yList1)
+        n=len(yList1)
+        xList=np.arange(n)
+
+        width=0.7
+        bar1.append(ax.bar(xList,yList1,width,color=self.get_color(0,N+1)))
+        #ax.bar(xList,yList,width,color=matplotlib.colors.colorConverter.to_rgb('0.3'))
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,yList1.max()*1.1)
+        ax.set_xticks(xList+width/2)
+        ax.set_xticklabels([])
+
+        if yTitle1==0 :
+            yTitle1='Mutations(n)'
+            ax.set_ylabel(yTitle1)
+ 
+        if legTitle==0 :
+            #legTitle=['type'+str(i) for i in range(1,len(bar1+bar2)+1)]
+            legTitle=['T>G/A>C','T>C/A>G','T>A/A>T','C>A/G>T','C>G/G>C','C>T/G>A','SNV']
+        if legX==0 :
+            legX=0.5
+        if legY==0 :
+            legY=1.0
+
+        legBar=[]
+        #legBar=[bar[i][0] for i in range(len(bar1+bar2))]
+        for i in range(len(bar2)) :
+            legBar.append(bar2[i][0])
+        for i in range(len(bar1)) :
+            legBar.append(bar1[i][0])
+
+        #ax.legend(legBar,legTitle,ncol=4,shadow=True,loc='upper right',bbox_to_anchor=[legX,legY],prop={'size':20})
+        #ax.legend(legBar,legTitle,ncol=4,shadow=True,loc='lower center',bbox_to_anchor=[legX,legY],prop={'size':13})
+        ax.legend(legBar,legTitle,ncol=4,shadow=False,loc='lower center',bbox_to_anchor=[legX,legY],prop={'size':10})
+        #ax.legend(legBar,legTitle,ncol=4,shadow=True,loc=[0,yList1.max()*1.1])
+        plt.subplots_adjust(hspace=0.07)
+
+        self.show()
+
+
+    def multi_bar_multi_bar_vertical_proportion(self,yList1,yList2,xLabel=0,xTitle=0,yTitle1=0,yTitle2=0,legTitle=0,legX=0,legY=0) :
+        fig = plt.figure()
+
+        ax=fig.add_subplot(212)
+
+        yList2=np.array(yList2)
+        bar2=[]
+        n=len(yList2[0])
+        N=len(yList2)
+        cn=len(yList2)+len(yList1)
+        ci=0
+
+        xList=np.arange(n)
+
+        width=0.7
+
+        bar2.append(ax.bar(xList,yList2[0]/sum(yList2[0:]).astype('float'),width,color=self.get_color(0,cn)))
+        for i in range(1,N) :
+            ci+=1
+            bar2.append(ax.bar(xList,yList2[i]/sum(yList2[0:]).astype('float'),width,bottom=sum(yList2[0:i])/sum(yList2[0:]).astype('float'),color=self.get_color(ci,cn)))
+
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,1)
+
+        ax.set_xticks(xList+width/2)
+        ax.set_yticks([0.0,0.2,0.4,0.6,0.8,1.0])
+        ax.set_yticklabels(['0%','20%','40%','60%','80%','100%'])
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+        if yTitle2==0 :
+            yTitle2='SNV(%)'
+        ax.set_ylabel(yTitle2)
+
+
+
+        #####
+
+        ax=fig.add_subplot(211)
+        bar1=[]
+        yList1=np.array(yList1)
+        N=len(yList1)
+        n=len(yList1[0])
+        xList=np.arange(n)
+        width=0.7
+        bar1.append(ax.bar(xList,yList1[0],width,color=self.get_color(ci,cn)))
+        ci+=1
+        for i in range(1,N) :
+            bar1.append(ax.bar(xList,yList1[i],width,bottom=sum(yList1[0:i]),color=self.get_color(ci,cn)))
+            ci+=1
+
+        #ax.bar(xList,yList,width,color=matplotlib.colors.colorConverter.to_rgb('0.3'))
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,yList1.max()*1.1)
+        ax.set_xticks(xList+width/2)
+        ax.set_xticklabels([])
+
+        if yTitle1==0 :
+            yTitle1='Mutations(n)'
+            ax.set_ylabel(yTitle1)
+ 
+        if legTitle==0 :
+            #legTitle=['type'+str(i) for i in range(1,len(bar1+bar2)+1)]
+            legTitle=['SNV','INDEL','T>G/A>C','T>C/A>G','T>A/A>T','C>A/G>T','C>G/G>C','C>T/G>A']
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=1.2
+
+        legBar=[]
+        #legBar=[bar[i][0] for i in range(len(bar1+bar2))]
+        for i in range(len(bar1)) :
+            legBar.append(bar1[i][0])
+        for i in range(len(bar2)) :
+            legBar.append(bar2[i][0])
+
+        ax.legend(legBar,legTitle,ncol=4,shadow=True,loc='upper right',bbox_to_anchor=[legX,legY])
+        plt.subplots_adjust(hspace=0.07)
+
+        self.show()
+
+
+
+    '''
+    def single_bar_multi_bar_vertical_proportion(self,yList1,yList2,xLabel=0,xTitle=0,yTitle=0,legTitle=0,legX=0,legY=0) :
+        fig = plt.figure()
+
+        ax=fig.add_subplot(211)
+
+        yList1=np.array(yList1)
+        n=len(yList1)
+        xList=np.arange(n)
+
+        width=0.7
+        ax.bar(xList,yList1,width,color=self.get_color(1))
+        #ax.bar(xList,yList,width,color=matplotlib.colors.colorConverter.to_rgb('0.3'))
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,yList1.max()*1.1)
+        ax.set_xticks(xList+width/2)
+        ax.set_xticklabels([])
+
+        ax=fig.add_subplot(212)
+
+        yList2=np.array(yList2)
+        n=len(yList2[0])
+        N=len(yList2)
+
+        xList=np.arange(n)
+
+        width=0.7
+
+        bar=[]
+        bar.append(ax.bar(xList,yList2[0]/sum(yList2[0:]).astype('float'),width,color=self.get_color(0)))
+        for i in range(1,N) :
+            bar.append(ax.bar(xList,yList2[i],width,bottom=sum(yList2[0:i])/sum(yList2[0:]).astype('float'),color=self.get_color(i)))
+
+        ax.set_xlim(-0.3,n)
+        ax.set_ylim(0,1)
+
+        ax.set_xticks(xList+width/2)
+        ax.set_yticks([0.0,0.2,0.4,0.6,0.8,1.0])
+        ax.set_yticklabels(['0%','20%','40%','60%','80%','100%'])
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+
+
+        if legTitle==0 :
+            legTitle=['type'+str(i) for i in range(1,N+1)]
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=0.97
+
+        legBar=[bar[i][0] for i in range(N)]
+
+        ax.legend(legBar,legTitle,loc='upper right',bbox_to_anchor=[legX,legY])
+
+        self.show()
+
+    '''
+
+
+
+    def multi_error_bar(self,yList,errList,xLabel=0,xTitle=0,yTitle=0,legTitle=0,legX=0,legY=0) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+
+        yList=np.array(yList)
+        errList=np.array(errList)
+        n=len(yList[0])
+        N=len(yList)
+
+        xList=np.arange(n)
+
+        width=0.9/N
+
+        bar=[]
+        for i in range(N) :
+            bar.append(ax.bar(xList+i*width,yList[i],width,color=self.get_color(i,N),yerr=errList[i]))
+
+
+        ax.set_xlim(-0.1,n)
+        ax.set_ylim(0,(yList+errList).max()+1)
+        ax.set_xticks(xList+0.9/2)
+
+        if xLabel==0 :
+            xLabel=['bar'+str(i) for i in range(1,n+1)]
+        ax.set_xticklabels(xLabel)
+
+        if legTitle==0 :
+            legTitle=['type'+str(i) for i in range(1,N+1)]
+        if legX==0 :
+            legX=0.97
+        if legY==0 :
+            legY=0.97
+
+        legBar=[bar[i][0] for i in range(N)]
+
+        ax.legend(legBar,legTitle,loc='upper right',bbox_to_anchor=[legX,legY])
+
+        self.show()
+
+    def heatmap_matshow(self,aList,xLabel=0,yLabel=0) :
+        fig = plt.figure()
+        ax=fig.add_subplot(111)
+        aList=np.array(aList)
+
+        n=len(aList[0])
+        N=len(aList)
+
+        if xLabel==0 :
+            xLabel=['s'+str(i) for i in range(1,n+1)]
+
+        if yLabel==0 :
+            yLabel=['g'+str(i) for i in range(1,N+1)]
+
+        cax=ax.matshow(aList,cmap=plt.cm.bone_r)
+        #cax=ax.matshow(aList,cmap=plt.cm.Greens)
+
+        ax.set_xticks(range(n))
+        ax.set_yticks(range(N))
+
+        ax.set_xticklabels(xLabel,rotation='vertical')
+        ax.set_yticklabels(yLabel)
+
+        fig.colorbar(cax,ticks=range(aList.max()+1))
+        #self.show()
+        plt.savefig(self.filename,bbox_inches='tight')
+        
+    def box_plot(self,aList,xLabel=0,yLabel=0) :
+        fig=plt.figure()
+        ax=fig.add_subplot(111)
+        N=len(aList)
+        ax.boxplot(aList)
+        if xLabel==0 :
+            xLabel=['s'+str(i) for i in range(1,N+1)]
+        ax.set_xticklabels(xLabel)
+        ax.set_ylim(0,np.array(aList).max()*1.1)
+        self.show()
+
+    def venn_diagram(self,aList,setname1='A',setname2='B',setname3='C') :
+        from RscriptClass  import Rscript
+        N=len(aList)
+        if N==2 :
+            A=set(aList[0])
+            B=set(aList[1])
+
+            AB=len(A&B)
+            A_B=len(A-B)
+            B_A=len(B-A)
+
+            Rcode=r'''
+            library('Vennerable');
+            v=Venn(SetNames= c("%s","%s"), Weight=c(%f,%f,%f,%f)); 
+            pdf("%s");
+            plot(v);
+            dev.off()
+            '''%(setname1,setname2,0,A_B,B_A,AB,self.filename)
+            R=Rscript(Rcode)
+
+        if N==3 :
+            A=set(aList[0])
+            B=set(aList[1])
+            C=set(aList[2])
+          
+            ABC=len(A&B&C)
+            A_B_C=len(A-B-C)
+            B_A_C=len(B-A-C)
+            C_A_B=len(C-A-B)
+            AB_ABC=len(A&B-A&B&C)
+            AC_ABC=len(A&C-A&B&C)
+            BC_ABC=len(B&C-A&B&C)
+
+            Rcode=r'''
+            library('Vennerable');
+            v=Venn(SetNames= c("%s","%s","%s"), Weight=c(%f,%f,%f,%f,%f,%f,%f,%f)); 
+            pdf("%s");
+            plot(v);
+            dev.off()
+            '''%(setname1,setname2,setname3,0,A_B_C,C_A_B,AC_ABC,B_A_C,AB_ABC,BC_ABC,ABC,self.filename)
+            R=Rscript(Rcode)
+            
+
