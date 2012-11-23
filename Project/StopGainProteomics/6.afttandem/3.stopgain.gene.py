985afaea5088ef892bc60d72338d3e4d0066d83a
hanice <sunahnice@gmail.com>
2012-11-23 22:01:22 +0800

    crontab.20121123

--- Project/StopGainProteomics/6.afttandem/3.stopgain.gene.py
+++ Project/StopGainProteomics/6.afttandem/3.stopgain.gene.py
@@ -7,7 +7,7 @@ for line in inFile:
     #D.setdefault(gene,[])
     #D[gene].append(line)
     D.setdefault(gene,{})
-    k = fields[1].split('output_')[1].split('.')[0]
+    k = fields[0].split('output_')[1].split('.')[0]
     D[gene].setdefault(k,0)
     D[gene][k]+=1
 
@@ -20,10 +20,19 @@ inFile.close()
 
 ouFile = open('tandem.peptides.stopgain.candidates.gene', 'w')
 
-for k1 in D:
-    ouFile.write(k1+'\t')
-    for  k2 in D[k1]:
-        ouFile.write(k2 +'\t'+str(D[k1][k2])+'\t')
+d = D.items()
+d.sort(cmp=lambda x,y:cmp(len(x[1]),len(y[1])),reverse=True)
+
+for item in d : 
+    ouFile.write(item[0]+'\t')
+    for it in item[1]:
+        ouFile.write(it +'\t'+str(item[1][it])+'\t')
     ouFile.write('\n')
 
+
+#for k1 in D:
+#    ouFile.write(k1+'\t')
+#    for  k2 in D[k1]:
+#        ouFile.write(k2 +'\t'+str(D[k1][k2])+'\t')
+#    ouFile.write('\n')
 ouFile.close()
