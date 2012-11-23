498a4ab3aa35e6fb0f360a1b2ff9435d1efd655b
hanice <sunahnice@gmail.com>
2012-11-23 19:00:23 +0800

    crontab.20121123

--- Project/StopGainProteomics/5.tandem/tandemxml.py
+++ Project/StopGainProteomics/5.tandem/tandemxml.py
@@ -1,31 +1,8 @@
 from xml.etree.ElementTree import *
 import os
-'''
-ouFile=open('pep2mgf_liuxiaohui','w')
-dir='/netshare1/home1/people/hansun/Project/fudancls/Tandem/output'
-for f in os.listdir(dir) :
-    file=dir+os.sep+f
-    if file[-4:]=='.xml' :
-        tree = ElementTree()
-        tree.parse(file)
-        group=tree.findall('group')
-        for item in group :
-            if item.get('id') :
-                seq=[]
-                mgf=[]
-                s=item.findall('protein/peptide/domain')
-                for it in s :
-                    seq.append(it.get('seq'))
-                m=item.findall('group/note')
-                for it in m :
-                    mgf.append(it.text.strip())
-        
-                ouFile.write(f+'\t'+'\t'.join(mgf)+'\t'+'\t'.join(seq)+'\n')
-ouFile.close()
-'''
 
-ouFile=open('pep2mgf_chenchen','w')
-dir='/netshare1/home1/people/hansun/Project/fudancls/Tandem/output2'
+ouFile=open('tandem.peptides','w')
+dir='/netshare1/home1/people/hansun/StopGainProteomics/5.tandem/test'
 for f in os.listdir(dir) :
     file=dir+os.sep+f
     if file[-4:]=='.xml' :
@@ -36,12 +13,29 @@ for f in os.listdir(dir) :
             if item.get('id') :
                 seq=[]
                 mgf=[]
+                label=[]
+                info=[]
+                pep = []
+
+                b=item.findall('protein')
+                for it in b :
+                    seq.append(it.get('label'))
+
+                p=item.findall('protein/peptide')
+                for it in p :
+                    pep.append(''.join(it.text.strip().split()))
+
                 s=item.findall('protein/peptide/domain')
                 for it in s :
                     seq.append(it.get('seq'))
+                    info.append(it.get('start')+':'+it.get('end')+':'+it.get('expect')+':'
+                            +it.get('hyperscore')+':'+it.get('pre')+':'+it.get('post'))
+
                 m=item.findall('group/note')
                 for it in m :
                     mgf.append(it.text.strip())
         
-                ouFile.write(f+'\t'+'\t'.join(mgf)+'\t'+'\t'.join(seq)+'\n')
+                #ouFile.write(f+'\t'+'\t'.join(mgf)+'\t'+'\t'.join(seq)+'\n')
+                #ouFile.write(f+'\t'+'\t'.join(label)+'\t'+'\t'.join(info)+'\t'+'\t'.join(seq)+'\n')
+                ou
 ouFile.close()
