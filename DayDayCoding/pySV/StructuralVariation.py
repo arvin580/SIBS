df993e86a60047a3ea4e26b80cdc0c588294ac9a
hanice <sunahnice@gmail.com>
2012-11-30 10:00:53 +0800

    crontab.20121130

--- DayDayCoding/pySV/StructuralVariation.py
+++ DayDayCoding/pySV/StructuralVariation.py
@@ -1,24 +1,44 @@
 import subprocess
-mapped_flags = [83, 99, 147, 163]
+import os
+
+
+mapped_both_flags = [83, 99, 147, 163]
+mapped_wrong_insertsize = [81, 161, 97, 145, 65, 129, 113, 177]
+chrs = ['chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14',
+                'chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrY','chrM']
 
 class StructuralVariation():
     def __init__(self, bam, ref):
         self.bam = bam
         self.bam_unmapped = bam + '.unmapped'
         self.ref = ref
-        self._bam_unmapped()
+        #self._bam_unmapped()
+        self.translocation_paired()
 
-    def translocation(self):
+    def translocation_paired(self):
+        self._bam_mapped_wrong_insertsize()
+        self._bam_trans()
+    def inversion_paired(self):
         pass
 
-    def inversion(self):
+    def duplication_paired(self):
         pass
 
-    def duplication(self):
+    def deletion_paired(self):
         pass
 
-    def deletion(self):
-        pass
+    def _bam_mapped_wrong_insertsize(self):
+        bam_mapped_wrong_insertsize = self.bam_unmapped + '.mapped_wrong_insertsize'
+        inFile = open(self.bam_unmapped)
+        ouFile = open(bam_mapped_wrong_insertsize, 'w')
+        for line in inFile:
+            fields = line.split('\t')
+            if int(fields[1]) in mapped_wrong_insertsize and fields[2] in chrs:
+                ouFile.write(line)
+        inFile.close()
+        ouFile.close()
+
+    def _bam_trans(self):
 
     def _bam_unmapped(self):
         if self.bam.find('.bam') != -1:
@@ -26,7 +46,7 @@ class StructuralVariation():
             sp=subprocess.Popen(['samtools','view',self.bam], stdout = subprocess.PIPE)
             for line in sp.stdout:
                 fields = line.split('\t')
-                if int(fields[1]) not in mapped_flags:
+                if int(fields[1]) not in mapped_both_flags:
                     ouFile.write(line)
             ouFile.close()
 
@@ -34,9 +54,10 @@ class StructuralVariation():
             inFile = open(self.bam)
             ouFile = open(self.bam_unmapped, 'w')
             for line in inFile:
-                fields = line.split('\t')
-                if int(fields[1]) not in mapped_flags:
-                    ouFile.write(line)
+                if line.find('@') != 0  :
+                    fields = line.split('\t')
+                    if int(fields[1]) not in mapped_both_flags:
+                        ouFile.write(line)
             inFile.close()
             ouFile.close()
         else:
