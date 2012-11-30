0c11ec5087f301d3a05ec1219c27de9bcebe73bb
hanice <sunahnice@gmail.com>
2012-11-30 09:00:24 +0800

    crontab.20121130

--- DayDayCoding/pySV/StructuralVariation.py
+++ DayDayCoding/pySV/StructuralVariation.py
@@ -1,8 +1,11 @@
+mapped_flags = [83, 99, 147, 163]
+
 class StructuralVariation():
-    def __init__(self, bamFile):
-        self.bamFile = bamFile
-        print(self.bamFile)
-        pass
+    def __init__(self, bam, ref):
+        self.bam = bam
+        self.bam_unmapped = bam + '.unmapped'
+        self.ref = ref
+        self._bam_unmapped()
 
     def translocation(self):
         pass
@@ -15,3 +18,21 @@ class StructuralVariation():
 
     def deletion(self):
         pass
+
+    def _bam_unmapped(self):
+        if self.bam.find('.bam') != -1:
+
+        elif self.bam.find('.sam')!= -1:
+            inFile = open(self.bam)
+            ouFile = open(self.bam_unmapped, 'w')
+            for line in inFile:
+                line = line.strip()
+                fields = line.split('\t')
+                if int(fields[1]) not in mapped_flags:
+                    ouFile.write(line + '\n')
+            inFile.close()
+            ouFile.close()
+        else:
+            print('The input file seems not in bam or sam format!')
+            return
+
--- DayDayCoding/pySV/pySV.py
+++ DayDayCoding/pySV/pySV.py
@@ -3,7 +3,8 @@ from optparse import OptionParser
 from StructuralVariation import StructuralVariation
 
 parser = OptionParser()
-parser.add_option('-f', '--file', dest = 'bamFile', help = 'input bam file')
+parser.add_option('-f', '--file', dest = 'bam', help = 'input bam or sam file')
+parser.add_option('-r', '--refernce', dest = 'ref', help = 'reference genome, such as hg19')
 (options, args) = parser.parse_args()
 
-sv = StructuralVariation(options.bamFile)
+sv = StructuralVariation(options.bam, options.ref)
