bdfb539e331099744c73eda9241f52b127f442c8
hanice <sunahnice@gmail.com>
2012-12-07 00:30:38 +0800

    crontab.20121207

--- DayDayCoding/pySV/circos.py
+++ DayDayCoding/pySV/circos.py
@@ -10,10 +10,10 @@ while True:
         if line1.split('\t')[2]!='chrM' and line2.split('\t')[2]!='chrM':
             link += 1
             fields = line1.split('\t')
-            ouFile.write('trans' + str(link) + '\t' + fields[2] + '\t' + fields[3] + '\n')
-            link += 1
+            #ouFile.write('trans' + str(link) + '\t' + 'hs'+fields[2].split('chr')[1] + '\t' + str(int(fields[3])/1000000) + '\n')
+            ouFile.write('trans' + str(link) + '\t' + 'hs'+fields[2].split('chr')[1] + '\t' + fields[3] + '\t' + str(int(fields[3])+500) + '\n')
             fields = line2.split('\t')
-            ouFile.write('trans' + str(link) + '\t' + fields[2] + '\t' + fields[3] + '\n')
+            ouFile.write('trans' + str(link) + '\t' + 'hs'+fields[2].split('chr')[1] + '\t' + fields[3] + '\t' + str(int(fields[3])+500) + '\n')
 
     else:
         break
