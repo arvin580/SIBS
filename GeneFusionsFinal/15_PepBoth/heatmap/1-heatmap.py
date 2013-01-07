3e910e8f89bce03fc5ff36b87d669d3916bc3745
hanice <sunahnice@gmail.com>
2013-01-07 22:30:17 +0800

    crontab.20130107

--- GeneFusionsFinal/15_PepBoth/heatmap/1-heatmap.py
+++ GeneFusionsFinal/15_PepBoth/heatmap/1-heatmap.py
@@ -1,17 +1,20 @@
-inFile = open('Tandem.Omssa.Fusion.Splicing.gene.curate')
-LD = []
-geneList = []
-pepList = []
-for line in inFile:
-    line = line.strip()
-    fields = line.split('\t')
-    LD.append([int(x) for x in fields[2:4]])
-    geneList.append(fields[0])
-    pepList.append(fields[1])
-inFile.close()
-for x in LD:
-    print(x)
-for x in geneList:
-    print(x)
-for x in pepList:
-    print(x)
+from PyPlot.PyPlotClass import *
+def gene_heatmap(sampleNameList,ouF,figsize=0,rowList=[]):
+    inFile = open('Tandem.Omssa.Fusion.Splicing.gene.curate')
+    LD = []
+    geneList = []
+    pepList = []
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        LD.append([int(x) for x in fields[2:4]])
+        geneList.append(fields[0])
+        pepList.append(fields[1])
+    inFile.close()
+
+    pp=PyPlot(ouF)
+    pp.heatmap(LD,col=False,xLabel=sampleNameList,yLabel=geneList,xLabelVertical=True,grid=True,figsize=figsize)
+
+
+gene_heatmap(['X!Tandem','Omssa'],'Tandem.Omssa.Fusion.Splicing.gene.curate.heatmap.pdf',figsize=(8,12))
+
