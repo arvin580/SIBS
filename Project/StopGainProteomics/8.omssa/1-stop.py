3ae89bffc2ab8c6a2650e39a51fc145f92bc01c9
hanice <sunahnice@gmail.com>
2013-01-12 17:30:13 +0800

    crontab.20130112

--- Project/StopGainProteomics/8.omssa/1-stop.py
+++ Project/StopGainProteomics/8.omssa/1-stop.py
@@ -2,6 +2,8 @@ import sys
 D = {}
 inFile = open(sys.argv[1])
 ouFile = open(sys.argv[1]+'.stop','w')
+ouFile2 = open(sys.argv[1]+'.stop2','w')
+ouFile3 = open(sys.argv[1]+'.stop_reverse','w')
 for line in inFile:
     fields = line.split(',')
     pep = fields[2]
@@ -12,8 +14,30 @@ for line in inFile:
         D[fields[2]]+=1
 inFile.close()
 
+
+def protein():
+    D2 = {}
+    inFile = open('/netshare1/home1/people/hansun/StopGainProteomics/2.uniprot/human_uniprot_sprot.fa')
+    while True:
+        line1 = inFile.readline().strip()
+        line2 = inFile.readline().strip()
+        if line1:
+            fields = line1.split('|')
+            name = fields[1]
+            D2.setdefault(line2,[])
+            D2[line2].append(name)
+        else:
+            break
+    inFile.close()
+    return D2
+
+D2=protein()
+
 for k in D:
     ouFile.write(k+'\t'+str(D[k])+'\n')
 
-
+    for x in D2:
+        if x[-len(k):]==k:
+            ouFile2.write('\t'.join(D2[x])+'\t')
+    ouFile2.write('\n')
 
