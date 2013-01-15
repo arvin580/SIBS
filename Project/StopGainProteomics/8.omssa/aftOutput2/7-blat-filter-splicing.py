fecf9c577c78fb70cda5f14462a2d090380b31b1
hanice <sunahnice@gmail.com>
2013-01-16 04:00:47 +0800

    crontab.20130116

--- Project/StopGainProteomics/8.omssa/aftOutput2/7-blat-filter-splicing.py
+++ Project/StopGainProteomics/8.omssa/aftOutput2/7-blat-filter-splicing.py
@@ -1,5 +1,17 @@
 inFile = open('dna_protein_out1.anno')
-con = inFile.read()
+D = {}
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    if len(fields)>5:
+        pep = fields[5]
+        D.setdefault(pep,[])
+    else:
+        D[pep].append(fields[0])
+        D[pep].append(fields[1])
+        D[pep].append(fields[2])
+        D[pep].append(fields[3])
+        D[pep].append(fields[4])
 inFile.close()
 
 inFile = open('3-stopgain-protein-unique2-filtered.blated.filtered3')
@@ -9,10 +21,14 @@ for line in inFile:
     line = line.strip()
     fields = line.split('\t')
     pep = fields[0].split(':')[1]
-    if con.find(pep)!=-1:
-        ouFile.write(line+'\n')
-    else:
+    flag = 0
+    for k in D:
+        if k.find(pep)!=-1:
+            ouFile.write(line+'\t'+k+'\t'+'\t'.join(D[k])+'\n')
+            flag = 1
+    if not flag:
         ouFile2.write(line+'\n')
+    
 
 inFile.close()
 ouFile.close()
