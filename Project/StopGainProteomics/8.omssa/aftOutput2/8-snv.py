be4ff8c043ab07f467eb1d74f2952f5ba3bc8155
hanice <sunahnice@gmail.com>
2013-01-16 07:30:27 +0800

    crontab.20130116

---
+++ Project/StopGainProteomics/8.omssa/aftOutput2/8-snv.py
@@ -0,0 +1,90 @@
+import string
+'''
+def read_seq():
+    inFile = open('/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta.fa')
+    D = {}
+    while True:
+        line1 = inFile.readline().strip()
+        line2 = inFile.readline().strip()
+        if line1:
+            D[line1.strip('>')]=line2
+        else:
+            break
+    inFile.close()
+    return D
+
+D = read_seq()
+
+def diff(codon):
+    CODON=['TAA','TAG','TGA']
+    SNV = []
+    for c in CODON:
+        n = 0
+        p = 0
+        for i in range(len(c)):
+            if c[i]==codon[i].upper():
+                n += 1
+            else:
+                p = i
+        if n == 2:
+            SNV.append([codon,c,p,codon[p],c[p]])
+    return SNV
+
+
+
+def seq(ch,pos,strand):
+    s = []
+    if strand=='+':
+        codon= D[ch][pos:pos+3]
+        df = diff(codon)
+        if df:
+            for item in df:
+                s.append([ch,pos+item[2],item[3],item[4],'+',item[0],item[1]])
+    elif strand=='-':
+        trans= string.maketrans('atcgATCG','tagcTAGC')
+        codon = D[ch][pos:pos+3]
+        codon = string.translate(codon[::-1],trans)
+        df = diff(codon)
+        if df:
+            for item in df:
+                s.append([ch,pos+2-item[2],string.translate(item[3],trans),string.translate(item[4],trans),'-',item[0],item[1]])
+    return s
+
+'''
+def snv(inF):
+    inFile = open(inF)
+    ouFile = open(inF+'.snv','w')
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        ch = fields[11]
+        start = int(fields[14])
+        pep = fields[0].split(':')[1]
+        ind = fields[12].index(pep)
+        #end = int(fields[12+(ind+len(pep))*5])
+        end = fields[12+(ind+len(pep))*5+2]
+        '''
+        if start <= end:
+            strand = '+'
+        else:
+            strand = '-'
+        if strand == '+':
+            pos = end
+            #s = seq(ch,pos,strand)
+        elif strand == '-':
+            pos = end - 4
+            #s = seq(ch,pos,strand)
+        '''    
+        print(start)
+        print(end)
+        s=[]
+        if s:    
+            ouFile.write('\t'.join(fields[0:13])+'\t')
+            for item in s:
+                ouFile.write('\t'.join([str(x) for x in item])+'\t')
+            ouFile.write('\n')
+    inFile.close()
+    ouFile.close()
+
+#snv('3-stopgain-protein-unique2-filtered.blated.filtered')
+snv('ha')
