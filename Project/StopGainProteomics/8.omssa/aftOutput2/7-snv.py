96ca87d1c2b146b84d4b4318888516070746d585
hanice <sunahnice@gmail.com>
2013-01-15 21:30:27 +0800

    crontab.20130115

--- Project/StopGainProteomics/8.omssa/aftOutput2/7-snv.py
+++ Project/StopGainProteomics/8.omssa/aftOutput2/7-snv.py
@@ -14,6 +14,7 @@ def read_seq():
     return D
 
 D = read_seq()
+print(len(D))
 
 def diff(codon):
     CODON=['TAA','TAG','TGA']
@@ -38,14 +39,14 @@ def seq(ch,pos,strand):
         codon= D[ch][pos:pos+3]
         df = diff(codon)
         if df:
-            s = [ch,pos+df[2],df[3],df[4],df[0],df[1]]
+            s = [ch,pos+df[0][2],df[0][3],df[0][4],df[0][0],df[0][1]]
     elif strand=='-':
         trans= string.maketrans('atcgATCG','tagcTAGC')
         codon = D[ch][pos:pos+3]
         codon = string.translate(condon[::-1],trans)
         df = diff(codon)
         if df:
-            s = [ch,pos+2-df[2],string.translate(df[3]),string.translate(df[4]),df[0],df[1]]
+            s = [ch,pos+2-df[0][2],string.translate(df[0][3]),string.translate(df[0][4]),df[0][0],df[0][1]]
     return s
 
 
