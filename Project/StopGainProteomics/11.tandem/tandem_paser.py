3eb56019cdb591e90a4e1c3bc35ac8cc0e8422fb
hanice <sunahnice@gmail.com>
2013-01-27 08:00:27 +0800

    crontab.20130127

---
+++ Project/StopGainProteomics/11.tandem/tandem_paser.py
@@ -0,0 +1,56 @@
+from xml.etree.ElementTree import *
+import os
+
+ouFile=open('tandem.peptides','w')
+dir='/netshare1/home1/people/hansun/StopGainProteomics/11.tandem/output'
+for f in os.listdir(dir) :
+    file=dir+os.sep+f
+    if file[-4:]=='.xml' :
+        tree = ElementTree()
+        tree.parse(file)
+        group=tree.findall('group')
+        for item in group :
+            if item.get('id') :
+                seq=[]
+                mgf=[]
+                label=[]
+                info=[]
+                pep = []
+
+                b=item.findall('protein')
+                for it in b :
+                    label.append(it.get('label'))
+
+                p=item.findall('protein/peptide')
+                for it in p :
+                    pep.append(''.join(it.text.strip().split()))
+
+                s=item.findall('protein/peptide/domain')
+                for it in s :
+                    seq.append(it.get('seq'))
+                    info.append(it.get('start')+':'+it.get('end')+':'+it.get('expect')+':'
+                            +it.get('hyperscore')+':'+it.get('pre')+':'+it.get('post'))
+
+                m=item.findall('group/note')
+                for it in m :
+                    mgf.append(it.text.strip())
+        
+                #ouFile.write(f+'\t'+'\t'.join(mgf)+'\t'+'\t'.join(seq)+'\n')
+                #ouFile.write(f+'\t'+'\t'.join(label)+'\t'+'\t'.join(info)+'\t'+'\t'.join(seq)+'\n')
+
+                if len(mgf)==1 and len(seq)==len(pep)==len(label)==len(info):
+                    ouFile.write(f+'\t'+'\t'.join(mgf)+'\t')
+                    for i in range(len(seq)):
+                        ouFile.write(seq[i]+'\t')
+                        ouFile.write(pep[i]+'\t')
+                        ouFile.write(label[i]+'\t')
+                        ouFile.write(info[i]+'\t')
+                    ouFile.write('\n')
+                else:
+                    print('>'+ f )
+                    print(mgf)
+                    print(seq)
+                    print(pep)
+                    print(label)
+                    print(info)
+ouFile.close()
