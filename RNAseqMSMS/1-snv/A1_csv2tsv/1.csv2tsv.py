71a1d3e546dc4d7ccd3a10978691e45da62a03c6
hanice <sunahnice@gmail.com>
2013-01-27 19:00:26 +0800

    crontab.20130127

---
+++ RNAseqMSMS/1-snv/A1_csv2tsv/1.csv2tsv.py
@@ -0,0 +1,19 @@
+import csv
+
+
+def csv2tsv(iFile):
+    inFile = open(iFile)
+    ouFile1 = open(iFile.rstrip('.csv'), 'w')
+    ouFile2 = open(iFile.rstrip('.csv')+'.indel', 'w')
+    csvFile = csv.reader(inFile)
+    head = csvFile.next()
+    for fields in csvFile:
+        if fields[33].find('INDEL')==0:
+            ouFile2.write('\t'.join(fields) + '\n')
+        else:
+            ouFile1.write('\t'.join(fields) + '\n')
+    inFile.close()
+    ouFile1.close()
+    ouFile2.close()
+
+csv2tsv('sum_snv.exome_summary.csv')
