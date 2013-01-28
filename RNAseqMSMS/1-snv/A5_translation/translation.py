340b1d55c145c962837b041666ad497a11e4835b
hanice <sunahnice@gmail.com>
2013-01-28 19:30:23 +0800

    crontab.20130128

--- RNAseqMSMS/1-snv/A5_translation/translation.py
+++ RNAseqMSMS/1-snv/A5_translation/translation.py
@@ -1,3 +1,5 @@
+import string
+
 def codon():
     D = {}
     inFile = open('CodonUsage')
@@ -10,23 +12,73 @@ def codon():
 
 def translate(seq,frame=''):
     seq = seq.upper()
+    trans = string.maketrans('ATCGatcg','TAGCtagc')
+    seq_r = string.translate(seq[::-1],trans)
+
     D = codon()
     P = []
     if frame == 0:
         for i in range(0,len(seq),3):
-            #P.append(D[seq[i:i+3]])
-            print(D[seq[i:i+3]])
+            if len(seq[i:i+3]) == 3:
+                if seq[i:i+3] in D:
+                    P.append(D[seq[i:i+3]])
+                else:
+                    print('0-frame translation error in ' + seq)
+                    break
+        print(''.join(P))
     if frame == 1:
-        pass
-    if frame == 2:
-        pass
-    if frame == -1:
-        pass
-    if frame == -2:
-        pass
-    if frame == -3:
-        pass
-    if frame == '':
-        pass
+        for i in range(1,len(seq),3):
+            if len(seq[i:i+3]) == 3:
+                if seq[i:i+3] in D:
+                    P.append(D[seq[i:i+3]])
+                else:
+                    print('1-frame translation error in ' + seq)
+                    break
+        print(''.join(P))
 
-translate('ATCGGATCGCTGGAT',0)
+    if frame == 2:
+        for i in range(2,len(seq),3):
+            if len(seq[i:i+3]) == 3:
+                if seq[i:i+3] in D:
+                    P.append(D[seq[i:i+3]])
+                else:
+                    print('2-frame translation error in ' + seq)
+                    break
+        print(''.join(P))
+
+    if frame == -1:
+        for i in range(0,len(seq_r),3):
+            if len(seq_r[i:i+3]) == 3:
+                if seq_r[i:i+3] in D:
+                    P.append(D[seq_r[i:i+3]])
+                else:
+                    print('-1-frame translation error in ' + seq_r)
+                    break
+        print(''.join(P))
+
+    if frame == -2:
+        for i in range(1,len(seq_r),3):
+            if len(seq_r[i:i+3]) == 3:
+                if seq_r[i:i+3] in D:
+                    P.append(D[seq_r[i:i+3]])
+                else:
+                    print('-2-frame translation error in ' + seq_r)
+                    break
+        print(''.join(P))
+
+    if frame == -3:
+        for i in range(2,len(seq_r),3):
+            if len(seq_r[i:i+3]) == 3:
+                if seq_r[i:i+3] in D:
+                    P.append(D[seq_r[i:i+3]])
+                else:
+                    print('-3-frame translation error in ' + seq_r)
+                    break
+        print(''.join(P))
+
+P=translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',0)
+P=translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',1)
+P=translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',2)
+P=translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',-1)
+P=translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',-2)
+P=translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',-3)
