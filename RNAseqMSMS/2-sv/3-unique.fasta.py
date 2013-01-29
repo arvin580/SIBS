53b264ef6fe0317e15b8facb061d8d1a73cd9d4b
hanice <sunahnice@gmail.com>
2013-01-29 09:00:23 +0800

    crontab.20130129

--- RNAseqMSMS/2-sv/3-unique.fasta.py
+++ RNAseqMSMS/2-sv/3-unique.fasta.py
@@ -1,15 +1,18 @@
 inFile = open('ERR0498-04-05.unmapped')
-ouFile = open('ERR0498-04-05.unmapped.unique.fasta','w')
 D = {}
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
     D[fields[9]]=fields[0]
 inFile.close()
-n = 0
-for k in D:
-    n += 1
-    
-    ouFile.write('>'+D[k]+'\n')
-    ouFile.write(k+'\n')
-ouFile.close()
+N = 100000
+d = D.items()
+
+for i in range(0,len(d),N): 
+    f = i/N
+    ouFile = open('ERR0498-04-05.unmapped.unique.'+str(f)+'.fasta','w')
+    for j in range(N):
+        if i+j < len(d):
+            ouFile.write('>'+d[i+j][1]+'\n')
+            ouFile.write(d[i+j][0]+'\n')
+    ouFile.close()
