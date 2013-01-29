4dc036d00f8c8454dd43ab25acd868a13a59ad3a
hanice <sunahnice@gmail.com>
2013-01-29 10:30:27 +0800

    crontab.20130129

---
+++ RNAseqMSMS/2-sv/ERR0498-04-05-fasta/ERR0498-04-05.unmapped.unique.153.blat.sh
@@ -0,0 +1,5 @@
+cd /netshare1/home1/people/hansun/RNAseqMSMS/2-sv/ERR0498-04-05-fasta
+db=/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta
+query=ERR0498-04-05.unmapped.unique.153.fasta
+out=${query}.blated
+blat $db $query  -out=blast8 $out
