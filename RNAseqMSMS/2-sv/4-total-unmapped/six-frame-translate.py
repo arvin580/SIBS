3e096fbea368c0add5f6793ff963aa46198d1e17
hanice <sunahnice@gmail.com>
2013-03-04 21:30:24 +0800

    crontab.20130304

---
+++ RNAseqMSMS/2-sv/4-total-unmapped/six-frame-translate.py
@@ -0,0 +1,8 @@
+Codon = {}
+inFile = open('/netshare1/home1/people/hansun/Data/CodonUsage')
+for line in inFile:
+    line = line.strip()
+    fields = line.split()
+    Codon[fields[0]]=fields[1]
+inFile.close()
+print(len(Codon))
