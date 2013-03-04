eca94ef113b3e02913efaff204ca8b3a503625b3
hanice <sunahnice@gmail.com>
2013-03-04 23:00:24 +0800

    crontab.20130304

--- RNAseqMSMS/2-sv/4-total-unmapped/unmapped-reads.sh
+++ RNAseqMSMS/2-sv/4-total-unmapped/unmapped-reads.sh
@@ -3,5 +3,5 @@ ls *.blated |xargs -n 1 python  1-unmapped.py
 ls *.unmapped |xargs -n 1 python six-frame-translation.py
 ls *.pep |xargs -n 1 python 2-nonstop.py
 ls *.nonstop |xargs -n 1 python 3-digestion.py
-ls *.digested |xargs -n 1 python 3-len.py
+ls *.digested |xargs -n 1 python 4-len.py
 
