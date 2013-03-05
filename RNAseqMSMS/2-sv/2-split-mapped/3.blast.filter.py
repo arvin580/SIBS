44b8c844777129c78aa442e845d156057b7b7532
hanice <sunahnice@gmail.com>
2013-03-06 00:30:25 +0800

    crontab.20130306

--- RNAseqMSMS/2-sv/2-split-mapped/3.blast.filter.py
+++ RNAseqMSMS/2-sv/2-split-mapped/3.blast.filter.py
@@ -29,7 +29,7 @@ def grep(s,ouFile):
 def unique(item):
     flag = [0, 0]
     for x in item[1][1:]:
-        if (int(x[6]) + int(x[7]))/2 <=50:
+        if (int(x[6]) + int(x[7]))/2 <=35:
             flag[0]+=1
         else:
             flag[1]+=1
@@ -58,13 +58,23 @@ def count(inF):
     ouFile1 = open(sys.argv[1]+'.filtered.head1','w')
     ouFile2 = open(sys.argv[1]+'.filtered.head2','w')
     ouFile3 = open(sys.argv[1]+'.filtered.seq1','w')
-    ##ouFile4 = open(sys.argv[1]+'.filtered.seq2','w')
+    ouFile4 = open(sys.argv[1]+'.filtered.seq2','w')
     ##ouFile5 = open(sys.argv[1]+'.filtered.grep','w')
     for item in d:
-        if item[1][0] == hits  and max([int(x[3]) for x in item[1][1:]])<80:
+        t = []
+        for x in item[1][1:]:
+            t.append('\t'.join(x))
+
+
+        if item[1][0] == hits  and max([int(x[3]) for x in item[1][1:]])<60:
             #flag = [0,0]
             flag = unique(item)
             if flag[0]<=1 and flag[1]<=1:
+                t = []
+                for x in item[1][1:]:
+                    t.append('\t'.join(x))
+
+                ouFile3.write('>'+item[0]+'\t'+str(item[1][0])+'\t'+'\t'.join(t)+'\n')
                 ouFile3.write(sv[item[0]]+'\n')
                 ouFile1.write(item[0]+'\t'+str(item[1][0])+'\n')
                 ##ouFile3.write(item[0]+'\n')
@@ -74,7 +84,9 @@ def count(inF):
         ##for x in sv[item[0]]:
         ##    ouFile4.write(x+'\n')
         ##ouFile4.write(item[0]+'\n')
-
+        ouFile4.write('>'+item[0]+'\t'+str(item[1][0])+'\t'+'\t'.join(t)+'\n')
+        ouFile4.write(sv[item[0]]+'\n')
+         
     ouFile1.close()
     ouFile2.close()
     ouFile3.close()
