1fb2cc9733b894311f122265c696e0c397d6ea25
hanice <sunahnice@gmail.com>
2013-03-07 01:00:30 +0800

    crontab.20130307

--- RNAseqMSMS/2-sv/3-unmapped-viruses/4-blat.py
+++ RNAseqMSMS/2-sv/3-unmapped-viruses/4-blat.py
@@ -4,8 +4,8 @@ files = os.listdir(DIR)
 for f in files:
     if f.find('.unmapped')!=-1:
         ouFile = open(DIR+'/'+f+'.blat.sh','w')
-        ouFile.write('cd /netshare1/home1/people/hansun/RNAseqMSMS/2-sv/ERR0498-04-05-fasta\n')
-        ouFile.write('db=/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta\n')
+        ouFile.write('cd /netshare1/home1/people/hansun/RNAseqMSMS/2-sv/3-unmapped-viruses\n')
+        ouFile.write('db=/netshare1/home1/people/hansun/Data/VirusesGenome/VirusesGenome.fasta\n')
         ouFile.write('query='+f+'\n')
         ouFile.write('out=${query}.blated\n')
         #ouFile.write('blastn  -db $db  -query $query -out $out -outfmt 6\n')
---
+++ RNAseqMSMS/2-sv/3-unmapped-viruses/ERR0498-04-05.unmapped.unique.10.fasta.blated.unmapped.blat.sh
@@ -0,0 +1,5 @@
+cd /netshare1/home1/people/hansun/RNAseqMSMS/2-sv/3-unmapped-viruses
+db=/netshare1/home1/people/hansun/Data/VirusesGenome/VirusesGenome.fasta
+query=ERR0498-04-05.unmapped.unique.10.fasta.blated.unmapped
+out=${query}.blated
+blat $db $query  -out=blast8 $out
