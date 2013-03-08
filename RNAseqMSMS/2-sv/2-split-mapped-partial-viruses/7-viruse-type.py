edabb3d8d1b142f08c03719a8f7c56b3d1fe37f8
hanice <sunahnice@gmail.com>
2013-03-08 23:00:22 +0800

    crontab.20130308

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/7-viruse-type.py
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/7-viruse-type.py
@@ -1,32 +1,30 @@
 import sys
 D = {}
-inFile = open(sys.argv[1])
-ouFile1 = open(sys.argv[1]+'.type-viruse', 'w')
-ouFile2 = open(sys.argv[1]+'.type-chr', 'w')
-while True:
-    line1 = inFile.readline().strip()
-    line2 = inFile.readline().strip()
-    if line1:
-        fields = line1.split()
-        if (fields[3].find('chr')==0 and fields[15].find('NC') == 0) or \
-            (fields[15].find('chr')==0 and fields[3].find('NC') == 0):
-            D.setdefault(fields[3], 0)
-            D.setdefault(fields[15], 0)
-            D[fields[3]]+=1
-            D[fields[15]]+=1
-    else:
-        break
 
-inFile.close()
-
-d = D.items()
-d.sort(cmp=lambda x,y:cmp(x[1],y[1]))
-
-for item in d:
-    if item[0].find('chr')==0:
-        ouFile2.write(item[0] + '\t' + str(item[1]) + '\n')
-    if item[0].find('NC')==0:
+def viruse(v):
+    inFile = open('ERR0498-04-05.unmapped.unique.human-viruse')
+    ouFile1 = open('ERR0498-04-05.unmapped.unique.human-viruse.'+v, 'w')
+    while True:
+        line1 = inFile.readline().strip()
+        line2 = inFile.readline().strip()
+        if line1:
+            fields = line1.split()
+            if (fields[3].find('chr')==0 and fields[15].find(v) == 0) or \
+                (fields[15].find('chr')==0 and fields[3].find(v) == 0):
+                D.setdefault(fields[3], 0)
+                D.setdefault(fields[15], 0)
+                D[fields[3]]+=1
+                D[fields[15]]+=1
+        else:
+            break
+    
+    inFile.close()
+    
+    d = D.items()
+    d.sort(cmp=lambda x,y:cmp(x[1],y[1]))
+    
+    for item in d:
         ouFile1.write(item[0] + '\t' + str(item[1]) + '\n')
+    ouFile1.close()
 
-ouFile1.close()
-ouFile2.close()
+viruse('NC_001357.1')
