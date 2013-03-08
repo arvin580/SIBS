cacfc947d5fc46c1f6efab67b38442d0c191d7c9
hanice <sunahnice@gmail.com>
2013-03-08 21:30:26 +0800

    crontab.20130308

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/qsub.10.sh
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/qsub.10.sh
@@ -1,6 +1,6 @@
-qsub ERR0498-04-05.unmapped.unique.167.blat.sh
-qsub ERR0498-04-05.unmapped.unique.77.blat.sh
-qsub ERR0498-04-05.unmapped.unique.3.blat.sh
-qsub ERR0498-04-05.unmapped.unique.94.blat.sh
-qsub ERR0498-04-05.unmapped.unique.52.blat.sh
-qsub ERR0498-04-05.unmapped.unique.86.blat.sh
+qsub ERR0498-04-05.unmapped.unique.167.blat.sh -l nodes=n4 
+qsub ERR0498-04-05.unmapped.unique.77.blat.sh -l nodes=n5 
+qsub ERR0498-04-05.unmapped.unique.3.blat.sh -l nodes=n6 
+qsub ERR0498-04-05.unmapped.unique.94.blat.sh -l nodes=n7 
+qsub ERR0498-04-05.unmapped.unique.52.blat.sh -l nodes=n9 
+qsub ERR0498-04-05.unmapped.unique.86.blat.sh -l nodes=n10 
---
+++ RNAseqMSMS/2-sv/3-unmapped-chimpanzee-mouse/1-merge.py
@@ -0,0 +1,42 @@
+import os
+
+D2 = {}
+files = os.listdir('.')
+for f in files:
+    if f[-9:]=='.unmapped':
+        inFile = open(f)
+        while True:
+            line1 = inFile.readline().strip('>\n')
+            line2 = inFile.readline().strip()
+            if line1:
+                D2[line1]=line2
+            else:
+                break
+        inFile.close()
+
+
+
+
+
+D = {}
+
+ouFile = open('unmapped-blated-chimpanzee-90-60-mouse-90-60', 'w')
+
+inFile = open('unmapped-blated-chimpanzee-90-60')
+for line in inFile:
+    line = line.strip()
+    fields = line.split()
+    D.setdefault(fields[0], [])
+    D[fields[0]].append(line)
+inFile.close()
+inFile = open('unmapped-blated-mouse-90-60')
+for line in inFile:
+    line = line.strip()
+    fields = line.split()
+    D.setdefault(fields[0], [])
+    D[fields[0]].append(line)
+inFile.close()
+
+for k in D:
+    ouFile.write('\t'.join(D[k])+'\n')
+    ouFile.write('\t'.join(D2[k])+'\n')
