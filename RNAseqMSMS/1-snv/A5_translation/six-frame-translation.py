0a4e17a6b0e158baf72fd91f732c44c72e76aee6
hanice <sunahnice@gmail.com>
2013-03-09 08:30:44 +0800

    crontab.20130309

--- RNAseqMSMS/1-snv/A5_translation/six-frame-translation.py
+++ RNAseqMSMS/1-snv/A5_translation/six-frame-translation.py
@@ -9,11 +9,22 @@ for line in inFile:
     Codon[fields[0]]=fields[1]
 inFile.close()
 
-def translate(seq):
+def translate(seq,start,end,FROM,TO):
+    # start,end:count from 1.
     six = []
     trans = string.maketrans('atcgATCG','tagcTAGC')
     seq1 = seq
-    seq2 = string.translate(seq[::-1],trans)
+    if seq[start-1:end]== FROM:
+        seq1_to = seq[0:start-1]+TO+seq[end:]
+        seq2 = string.translate(seq1[::-1],trans)
+        seq2_to = string.translate(seq1_to[::-1],trans)
+        print(seq1)
+        print(seq2)
+        print(seq1_to)
+        print(seq2_to)
+    else:
+        print('warning:'+'\t'+seq)
+    '''
     for i in range(3):
         pep = []
         for j in range(i,len(seq1),3):
@@ -30,8 +41,10 @@ def translate(seq):
         six.append(''.join(pep))
 
     return six
+    '''
 
 
 
-six = translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA')
+#six = translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',)
+six = translate('CCGAGGCAGGGATCAAA',8,10,'AGG','A')
 print(six)
