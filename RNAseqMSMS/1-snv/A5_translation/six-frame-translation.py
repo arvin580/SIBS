db972de5617e03203c7b746176b2378863b1eddf
hanice <sunahnice@gmail.com>
2013-03-09 23:30:35 +0800

    crontab.20130309

--- RNAseqMSMS/1-snv/A5_translation/six-frame-translation.py
+++ RNAseqMSMS/1-snv/A5_translation/six-frame-translation.py
@@ -14,6 +14,10 @@ def translate(seq,start,end,FROM,TO):
     # start,end:count from 1.
     six = []
     trans = string.maketrans('atcgATCG','tagcTAGC')
+    print(start)
+    print(end)
+    print(FROM)
+    print(TO)
     if seq[start-1:end]== FROM:
         seq_to = seq[0:start-1]+TO+seq[end:]
         seq_rev = string.translate(seq[::-1],trans)
@@ -73,10 +77,38 @@ def translate(seq,start,end,FROM,TO):
 #six = translate('CCGAGGCAGGGATCAAAGATCAGCCTA',11,11,'G','A')
 #print(six)
 
+L = 40
+
+D = {}
+inFile = open('/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta.fa')
+while True:
+    line1 = inFile.readline().strip('>\n')
+    line2 = inFile.readline().strip()
+    if line1:
+        D[line1]=line2
+    else:
+        break
+
+inFile.close()
+
 def snv(inF):
-    inFile = open('')
+    inFile = open(inF)
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        ch = fields[21]
+        start = int(fields[22])
+        end = int(fields[23])
+        FROM = fields[24]
+        TO = fields[25]
+        s = start - L -1
+        e = end + L
+        seq = D[ch][s:e]
+        six = translate(seq, L+1, L+len(FROM), FROM, TO)
+        print(six)
     inFile.close()
 
-
+#snv('sum_snv.exome_summary.nonsynonymous-splicing')
+snv('ha')
 
 
