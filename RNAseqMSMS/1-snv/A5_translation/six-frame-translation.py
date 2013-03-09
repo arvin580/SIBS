b90f3cb5163e5435e7ad32ea8b2c582776549dd2
hanice <sunahnice@gmail.com>
2013-03-09 22:30:37 +0800

    crontab.20130309

--- RNAseqMSMS/1-snv/A5_translation/six-frame-translation.py
+++ RNAseqMSMS/1-snv/A5_translation/six-frame-translation.py
@@ -1,5 +1,6 @@
 import sys
 import string
+from math import ceil
 
 Codon = {}
 inFile = open('/netshare1/home1/people/hansun/Data/CodonUsage')
@@ -13,37 +14,38 @@ def translate(seq,start,end,FROM,TO):
     # start,end:count from 1.
     six = []
     trans = string.maketrans('atcgATCG','tagcTAGC')
-    seq1 = seq
     if seq[start-1:end]== FROM:
-        seq1_to = seq[0:start-1]+TO+seq[end:]
-        seq2 = string.translate(seq1[::-1],trans)
-        seq2_to = string.translate(seq1_to[::-1],trans)
-        print(seq1)
-        print(seq2)
-        print(seq1_to)
-        print(seq2_to)
+        seq_to = seq[0:start-1]+TO+seq[end:]
+        seq_rev = string.translate(seq[::-1],trans)
+        seq_to_rev = string.translate(seq_to[::-1],trans)
+        print(seq)
+        print(seq_rev)
+        print(seq_to)
+        print(seq_to_rev)
+
+        for i in range(3):
+            pep = []
+            for j in range(i,len(seq),3):
+                c = seq[j:j+3]
+                if len(c) == 3:
+                    pep.append(Codon[c])
+            s = start - i
+            e = end - i 
+            print(s)
+            print(e)
+            six.append([''.join(pep),int(ceil(s/3)),int(ceil(e/3))])
+        for i in range(3):
+            pep = []
+            for j in range(i,len(seq_rev),3):
+                c = seq_rev[j:j+3]
+                if len(c) == 3:
+                    pep.append(Codon[c])
+            six.append([''.join(pep),0,0])
+    
+        return six
+
     else:
         print('warning:'+'\t'+seq)
-    '''
-    for i in range(3):
-        pep = []
-        for j in range(i,len(seq1),3):
-            c = seq1[j:j+3]
-            if len(c) == 3:
-                pep.append(Codon[c])
-        six.append(''.join(pep))
-    for i in range(3):
-        pep = []
-        for j in range(i,len(seq2),3):
-            c = seq2[j:j+3]
-            if len(c) == 3:
-                pep.append(Codon[c])
-        six.append(''.join(pep))
-
-    return six
-    '''
-
-
 
 #six = translate('CAGCACCTCCCTGATGGGGACAAAACGCCCATGTCCGAGCGGCTGGACGACACGGAGCCCTATTTCATCGGGATCTTTTGCTTCGAGGCAGGGATCAAA',)
 six = translate('CCGAGGCAGGGATCAAA',8,10,'AGG','A')
