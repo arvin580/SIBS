c505b58bc6f71d248cf94c94688060a3cea9ea54
hanice <sunahnice@gmail.com>
2013-03-10 18:30:26 +0800

    crontab.20130310

--- RNAseqMSMS/1-snv/A5_translation/ref-alt.py
+++ RNAseqMSMS/1-snv/A5_translation/ref-alt.py
@@ -13,6 +13,7 @@ def ref_alt(inF):
     inFile = open(inF)
     ouFile1 = open(inF+'.ref-alt','w')
     ouFile2 = open(inF+'.not-ref-alt','w')
+    ouFile3 = open(inF+'.ref-alt2','w')
     while True:
         L = []
         for i in range(24):
@@ -27,10 +28,14 @@ def ref_alt(inF):
                 for k in D:
                     if L[i+1] in D[k]:
                         flag += 1
-                        ouFile1.write(L[i]+'\t'+k+'\n')
-                        ouFile1.write(L[i+1]+'\n')
-                        ouFile1.write(L[i+2]+'\t'+k+'\n')
-                        ouFile1.write(L[i+3]+'\n')
+                        if L[i].find('REF')!=-1:
+                            ouFile1.write(L[i]+'\t'+k+'\n')
+                            ouFile1.write(L[i+1]+'\n')
+                            ouFile1.write(L[i+2]+'\t'+k+'\n')
+                            ouFile1.write(L[i+3]+'\n')
+                        else:
+                            ouFile3.write(L[i]+'\t'+k+'\n')
+                            ouFile3.write(L[i+1]+'\n')
             if not flag:
                 for item in L:
                     ouFile2.write(item+'\n')
@@ -41,6 +46,7 @@ def ref_alt(inF):
     inFile.close()
     ouFile1.close()
     ouFile2.close()
+    ouFile3.close()
 
 ref_alt('sum_snv.exome_summary.indel.overall.filter.pep')
 
