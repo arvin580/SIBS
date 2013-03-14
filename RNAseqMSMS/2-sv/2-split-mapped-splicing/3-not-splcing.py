4ed251a07ae014072dc438742dbfdf96a5519f5e
hanice <sunahnice@gmail.com>
2013-03-14 17:30:26 +0800

    crontab.20130314

--- RNAseqMSMS/2-sv/2-split-mapped-splicing/3-not-splcing.py
+++ RNAseqMSMS/2-sv/2-split-mapped-splicing/3-not-splcing.py
@@ -20,6 +20,7 @@ for f in files:
 
 DIR='../2-split-mapped3'
 files = os.listdir(DIR)
+D2= {}
 for f in files:
     if f[-13:]=='.not-splicing':
         inFile = open(DIR+'/'+f)
@@ -28,14 +29,26 @@ for f in files:
             line2 = inFile.readline().strip()
             if line1:
                 fields = line1.split('\t')
-                D.setdefault(fields[0],[])
-                D[fields[0]].append(line1)
-                D[fields[0]].append(line2)
+                D2.setdefault(fields[0],[])
+                D2[fields[0]].append(line1)
+                D2[fields[0]].append(line2)
             else:
                 break
         inFile.close()
-ouFile = open('ERR0498-04-05.unmapped.unique.total.fasta.blated.filtered.seq1.splicing.known-predict','w')
-for k in D:
-    ouFile.write('\t'.join(D[k][0::2])+'\n')
-    ouFile.write('\t'.join(D[k][1::2])+'\n')
+ouFile = open('ERR0498-04-05.unmapped.unique.total.fasta.blated.filtered.seq1.sv.known-predict','w')
 
+for f in files:
+    if f[-13:]=='.not-splicing':
+        inFile = open(DIR+'/'+f)
+        while True:
+            line1 = inFile.readline().strip()
+            line2 = inFile.readline().strip()
+            if line1:
+                fields = line1.split('\t')
+                if fields[0] in D and fields[0] in D2:
+                    ouFile.write(line1+'\n')
+                    ouFile.write(line2+'\n')
+            else:
+                break
+        inFile.close()
+ouFile.close()
