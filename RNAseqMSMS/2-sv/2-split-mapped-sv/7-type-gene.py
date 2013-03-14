4ee8b63bf92a9e6d3fb0dbf6b7e3181a869bf826
hanice <sunahnice@gmail.com>
2013-03-15 06:30:26 +0800

    crontab.20130315

--- RNAseqMSMS/2-sv/2-split-mapped-sv/7-type-gene.py
+++ RNAseqMSMS/2-sv/2-split-mapped-sv/7-type-gene.py
@@ -2,29 +2,40 @@ D = {}
 inFile = open('split-mapped-duplication.gene.gene')
 for line in inFile:
     line = line.strip()
-    fields = line.split()
+    fields = line.split('\t')
     D.setdefault(fields[0], 0)
-    D[fields[0]]=+1
+    D[fields[0]] += 1
 inFile.close()
 
 inFile = open('split-mapped-inversion.gene.gene')
 for line in inFile:
     line = line.strip()
-    fields = line.split()
+    fields = line.split('\t')
     D.setdefault(fields[0], 0)
-    D[fields[0]]=+1
+    D[fields[0]] += 1
 inFile.close()
 
 inFile = open('split-mapped-translocation.gene.gene')
 for line in inFile:
     line = line.strip()
-    fields = line.split()
+    fields = line.split('\t')
     D.setdefault(fields[0], 0)
-    D[fields[0]]=+1
+    D[fields[0]] += 1
 inFile.close()
 
-d = D.items()
-d.sort(cmp = lambda x,y:cmp(x,y), reverse = True)
+inFile = open('split-mapped-deletion.gene.gene')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    D.setdefault(fields[0], 0)
+    D[fields[0]] += 1
+inFile.close()
 
+
+
+d = D.items()
+d.sort(cmp = lambda x,y:cmp(x[1],y[1]), reverse = True)
+
+ouFile = open('split-mapped-deletion-inversion-duplication-translocation.gene.gene','w')
 for item in d:
-    print(item[0]+'\t'+str(item[1]))
+    ouFile.write(item[0]+'\t'+str(item[1])+'\n')
