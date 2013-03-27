30385838203316e97ceda133e69bcd6b89938ed3
hanice <sunahnice@gmail.com>
2013-03-27 23:30:25 +0800

    crontab.20130327

--- RNAseqMSMS/0-Mann/2-cleavge.py
+++ RNAseqMSMS/0-Mann/2-cleavge.py
@@ -4,9 +4,14 @@ def cleavge(inF):
     for line in inFile:
         fields = line.split('\t')
         if len(fields)>3:
-            D[fields[2][-1]]=1
+            if fields[2]:
+                D.setdefault(fields[2][-1],0)
+                D[fields[2][-1]]+=1
     inFile.close()
+    print(inF)
     print(D)
 
 cleavge('msb201181-s2-peptides-gluc.txt')
+cleavge('msb201181-s2-peptides-lysc.txt')
+cleavge('msb201181-s2-peptides-trypsin.txt')
         
