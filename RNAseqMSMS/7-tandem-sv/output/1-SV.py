3f3e7314b65c898964c35f72b573efd37ab2b7d1
hanice <sunahnice@gmail.com>
2013-03-30 07:00:26 +0800

    crontab.20130330

--- RNAseqMSMS/7-tandem-sv/output/1-SV.py
+++ RNAseqMSMS/7-tandem-sv/output/1-SV.py
@@ -3,12 +3,13 @@ import os
 files = os.listdir('.')
 ouFile = open('Homo_SV','w')
 for inF in files:
-    inFile = open(inF)
-    for line in inFile:
-        line = line.strip()
-        if line.find('SV:')!=-1 and line.find('ENSP')==-1:
-            ouFile.write(line+'\n')
-    inFile.close()
+    if inF[-4:]=='.txt':
+        inFile = open(inF)
+        for line in inFile:
+            line = line.strip()
+            if line.find('SV:')!=-1 and line.find('ENSP')==-1:
+                ouFile.write(line+'\n')
+        inFile.close()
 
 ouFile.close()
 
