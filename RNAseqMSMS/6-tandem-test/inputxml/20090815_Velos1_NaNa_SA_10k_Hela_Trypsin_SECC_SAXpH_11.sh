02cd41f6ac89906332e21cb90cd9abb53f31c550
hanice <sunahnice@gmail.com>
2013-03-30 01:00:28 +0800

    crontab.20130330

---
+++ RNAseqMSMS/6-tandem-test/inputxml/20090815_Velos1_NaNa_SA_10k_Hela_Trypsin_SECC_SAXpH_11.sh
@@ -0,0 +1,3 @@
+cd /netshare1/home1/people/hansun/RNAseqMSMS/6-tandem-test
+
+tandem.exe inputxml/20090815_Velos1_NaNa_SA_10k_Hela_Trypsin_SECC_SAXpH_11.xml
---
+++ RNAseqMSMS/6-tandem-test/inputxml/qsub.sh
@@ -0,0 +1,1 @@
+qsub -l nodes=cu11 20090815_Velos1_NaNa_SA_10k_Hela_Trypsin_SECC_SAXpH_11.sh
