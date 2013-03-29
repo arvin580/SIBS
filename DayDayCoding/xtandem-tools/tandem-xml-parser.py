f3047bd8c461a18797140957d05480adc6c70b1d
hanice <sunahnice@gmail.com>
2013-03-29 23:30:26 +0800

    crontab.20130329

--- DayDayCoding/xtandem-tools/tandem-xml-parser.py
+++ DayDayCoding/xtandem-tools/tandem-xml-parser.py
@@ -2,16 +2,19 @@ from xml.etree.ElementTree import *
 import sys
 
 class Spectrum:
-    name = ''
-    protein = []
+    def __init__(self):
+        self.name = ''
+        self.protein = []
 class Protein:
-    name = ''
-    seq = ''
-    attr = ''
-    peptide = []
+    def __init__(self):
+        self.name = ''
+        self.attr = ''
+        self.seq = ''
+        self.peptide = []
 class Peptide:
-    seq = ''
-    attr = ''
+    def __init__(self):
+        self.seq = ''
+        self.attr = ''
 
 
 inF = sys.argv[1]
@@ -32,10 +35,15 @@ for item in group :
         for x in pro :
             p = Protein()
             s = x.findall('note')
-            if len(s)==1:
-                p.name=s[0].text
+            if len(s) == 1:
+                p.name = s[0].text
             else:
                 print('protein error')
+            s = x.findall('peptide')
+            if len(s)==1:
+                p.attr = s[0].get('start')+':'+s[0].get('end')+':'+x.get('expect')
+            else:
+                print('protein peptide error')
             pep = x.findall('peptide/domain')
             for y in pep:
                 pe = Peptide()
@@ -44,48 +52,11 @@ for item in group :
                         +y.get('hyperscore')+':'+y.get('pre')+':'+y.get('post')
                 p.peptide.append(pe)
             Spec.protein.append(p)
-        '''
-        print(Spec.name+'\t'),
+
+        ouFile.write(Spec.name+'\t')
         for protein in Spec.protein:
             for peptide in protein.peptide:
-                #print(protein.name+'\t'+peptide.seq+'\t'+peptide.attr+'\t'),
-                #print(peptide.seq+'\t'+peptide.attr+'\t'),
-                print(protein.name[0:7]+'\t'+peptide.seq+'\t'),
-        print('\n'),
-        '''
+                ouFile.write(protein.name+'\t'+protein.attr+'\t'+peptide.seq+'\t'+peptide.attr+'\t')
+        ouFile.write('\n')
 
 ouFile.close()
-
-'''
-
-p=item.findall('protein/peptide')
-for it in p :
-    pep.append(''.join(it.text.strip().split()))
-
-s=item.findall('protein/peptide/domain')
-for it in s :
-    seq.append(it.get('seq'))
-    info.append(it.get('start')+':'+it.get('end')+':'+it.get('expect')+':'
-            +it.get('hyperscore')+':'+it.get('pre')+':'+it.get('post'))
-
-
-#ouFile.write(f+'\t'+'\t'.join(mgf)+'\t'+'\t'.join(seq)+'\n')
-#ouFile.write(f+'\t'+'\t'.join(label)+'\t'+'\t'.join(info)+'\t'+'\t'.join(seq)+'\n')
-
-if len(mgf)==1 and len(seq)==len(pep)==len(pro)==len(info):
-    ouFile.write(f+'\t'+'\t'.join(mgf)+'\t')
-    for i in range(len(seq)):
-        ouFile.write(seq[i]+'\t')
-        ouFile.write(pep[i]+'\t')
-        #ouFile.write(label[i]+'\t')
-        ouFile.write(pro[i]+'\t')
-        ouFile.write(info[i]+'\t')
-    ouFile.write('\n')
-else:
-    print('>'+ f )
-    print(mgf)
-    print(seq)
-    print(pep)
-    print(label)
-    print(info)
-'''
