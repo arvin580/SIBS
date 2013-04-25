827b1c03cf02feb148e4915c2e0ac51f7ee1f060
hanice <sunahnice@gmail.com>
2013-04-26 03:30:31 +0800

    crontab.20130426

---
+++ RNAseqMSMS/5-tandem-snv-indel/output-high-quality-filter/1-output-filter.py
@@ -0,0 +1,29 @@
+import os
+
+DIR = '../output-high-quality'
+files = os.listdir(DIR)
+
+ouFile1 = open('HeLa-SNV-INDEL-REF','w')
+ouFile2 = open('HeLa-SNV-INDEL-ALT','w')
+ouFile3 = open('HeLa-Known-Protein','w')
+ouFile4 = open('HeLa-SNV-INDEL-REVERSE','w')
+for f in files:
+    if f[-8:] == '.txt.fdr':
+        inFile = open(DIR + '/' + f)
+        for line in inFile:
+            if line.find('REVERSE')!=-1:
+                ouFile4.write(line)
+            elif line.find('chromosome') != -1:
+                ouFile3.write(line)
+            elif line.find('ALT')!=-1 and line.find('REF') == -1:
+                ouFile2.write(line)
+            if line.find('REF')!=-1:
+                ouFile1.write(line)
+        inFile.close()
+
+ouFile1.close()
+ouFile2.close()
+ouFile3.close()
+ouFile4.close()
+
+
