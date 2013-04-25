6944ad6b1976e216bd4c84a59c7f952148692473
hanice <sunahnice@gmail.com>
2013-04-26 06:00:37 +0800

    crontab.20130426

---
+++ RNAseqMSMS/5-tandem-snv-indel/output-high-quality-filter/3-gene.py
@@ -0,0 +1,38 @@
+L = []
+inFile  = open('/netshare1/home1/people/hansun/RNAseqMSMS/1-snv/A1_csv2tsv/sum_snv.exome_summary')
+for line in inFile:
+    line = line.strip()
+    L.append(line)
+inFile.close()
+
+inFile  = open('/netshare1/home1/people/hansun/RNAseqMSMS/1-snv/A1_csv2tsv/sum_snv.exome_summary.indel')
+for line in inFile:
+    line = line.strip()
+    L.append(line)
+inFile.close()
+
+
+def gene_dbsnp(inF):
+    inFile = open(inF)
+    ouFile = open(inF+'.gene','w')
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        tp = fields[1].split(':')[0]
+        ch = fields[1].split(':')[1]
+        pos = fields[1].split(':')[2]
+        genes = []
+        dbsnps = []
+        for item in L:
+            if item.find(ch) and item.find(pos):
+                gene = item.split('\t')[1]
+                dbsnp = item.split('\t')[8]
+                genes.append(gene)
+                dbsnps.append(dbsnp)
+        ouFile.write(fields[0]+'\t'+'|'.join(genes)+'\t'+'|'.join(dbsnps)+'\t'+fields[1]+'\n')
+
+
+    inFile.close()
+    ouFile.close()
+
+gene_dbsnp('HeLa-SNV-INDEL-ALT-pep')
