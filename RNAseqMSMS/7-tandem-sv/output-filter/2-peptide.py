9f59d8622991e06f65d7c633958445fd1fb9ade0
hanice <sunahnice@gmail.com>
2013-04-27 18:00:28 +0800

    crontab.20130427

--- RNAseqMSMS/7-tandem-sv/output-filter/2-peptide.py
+++ RNAseqMSMS/7-tandem-sv/output-filter/2-peptide.py
@@ -1,5 +1,6 @@
 def pep(inF):
     inFile = open(inF)
+    
     D = {}
     for line in inFile:
         line = line.strip()
@@ -7,10 +8,11 @@ def pep(inF):
         start = int(fields[4].split(':')[0])
         pre = fields[4].split(':')[5]
         post = fields[4].split(':')[6]
-        s = int(fields[1].split(':')[-3])
-        e = int(fields[1].split(':')[-2])
+        #s = int(fields[1].split(':')[-3])
+        #e = int(fields[1].split(':')[-2])
         D.setdefault(fields[3], [])
-        D[fields[3]].append(fields[1]+':'+str(s-start+1)+':'+str(e-start+1)+':'+pre+':'+post)
+        #D[fields[3]].append(fields[1]+':'+str(s-start+1)+':'+str(e-start+1)+':'+pre+':'+post)
+        D[fields[3]].append(fields[1]+':'+str(-1)+':'+str(-1)+':'+pre+':'+post)
     
     inFile.close()
     
@@ -18,6 +20,12 @@ def pep(inF):
     for k in D:
         v = '\t'.join(D[k])
         ouFile.write(k+'\t'+v+'\n')
+    
     ouFile.close()
 
 pep('HeLa-SV-Deletion')
+pep('HeLa-SV-Duplication')
+pep('HeLa-SV-Inversion')
+pep('HeLa-SV-Translocation')
+pep('HeLa-Predict-Splicing')
+pep('HeLa-Predict')
