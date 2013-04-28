fa49d210c96ab0e6e9b26fc663ddff4975ebf207
hanice <sunahnice@gmail.com>
2013-04-29 02:00:26 +0800

    crontab.20130429

--- RNAseqMSMS/2-sv/3-unmapped-viruses/viruses-sites-stat.py
+++ RNAseqMSMS/2-sv/3-unmapped-viruses/viruses-sites-stat.py
@@ -1,22 +1,26 @@
-def virus(inF, v, pos):
-    inFile = open(inF)
+def virus(inF, v, POS):
     ouFile = open(inF + '.stat', 'w')
-    before = 0
-    after = 0
-    while True:
-        line1 = inFile.readline().strip()
-        line2 = inFile.readline().strip()
-        if line1:
-            fields = line1.split('\t')
-            if fields[1] == v:
-                if int(fields[8]) < pos and int(fields[9]) < pos:
-                    before += 1
-                else:
-                    after += 1
-        else:
-            break
-    inFile.close()
-    print(before)
-    print(after)
+    for pos in POS:
+        inFile = open(inF)
+        before = 0
+        after = 0
+        while True:
+            line1 = inFile.readline().strip()
+            line2 = inFile.readline().strip()
+            if line1:
+                fields = line1.split('\t')
+                if fields[1] == v:
+                    if int(fields[8]) <= pos and int(fields[9]) <= pos:
+                        before += 1
+                    else:
+                        after += 1
+            else:
+                break
+        inFile.close()
+        ouFile.write('before ' + str(pos) +'\t' +str(before)+'\t' )
+        ouFile.write('after ' + str(pos) +'\t' +str(after)+'\n' )
+    ouFile.close()
+        
+        
 
-virus('unmapped-blated-viruses-90-60.seq', 'NC_001357.1', 3000)
+virus('unmapped-blated-viruses-90-60.seq', 'NC_001357.1', [1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500])
