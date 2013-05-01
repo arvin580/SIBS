8fcd675e33f7c259b22ac7f4fdb8be1323004d72
hanice <sunahnice@gmail.com>
2013-05-02 01:30:26 +0800

    crontab.20130502

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-HPV-Diagram/HPV-plot.py
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-HPV-Diagram/HPV-plot.py
@@ -55,37 +55,37 @@ E6 = [(105,-0.3),(105,-0.2),(581,-0.2),(581,-0.3),(0,0)]
 path = Path(E6, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((105+581)/2.0,-0.25,'E6',horizontalalignment='center',verticalalignment='center')
+ax.text((105+581)/2.0,-0.25,'E6',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E7 = [(590,-0.5),(590,-0.4),(907,-0.4),(907,-0.5),(0,0)]
 path = Path(E7, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((590+907)/2.0,-0.45,'E7',horizontalalignment='center',verticalalignment='center')
+ax.text((590+907)/2.0,-0.45,'E7',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E1 = [(914,-0.3),(914,-0.2),(2887,-0.2),(2887,-0.3),(0,0)]
 path = Path(E1, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((914+2887)/2.0,-0.25,'E1',horizontalalignment='center',verticalalignment='center')
+ax.text((914+2887)/2.0,-0.25,'E1',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E2 = [(2817,-0.5),(2817,-0.4),(3914,-0.4),(3914,-0.5),(0,0)]
 path = Path(E2, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((2817+3914)/2.0,-0.45,'E2',horizontalalignment='center',verticalalignment='center')
+ax.text((2817+3914)/2.0,-0.45,'E2',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E4 = [(3418,-0.3),(3418,-0.2),(3684,-0.2),(3684,-0.3),(0,0)]
 path = Path(E4, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((3418+3684)/2.0,-0.25,'E4',horizontalalignment='center',verticalalignment='center')
+ax.text((3418+3684)/2.0,-0.25,'E4',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E5 = [(3936,-0.3),(3936,-0.2),(4157,-0.2),(4157,-0.3),(0,0)]
 path = Path(E5, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((3936+4157)/2.0,-0.25,'E5',horizontalalignment='center',verticalalignment='center')
+ax.text((3936+4157)/2.0,-0.25,'E5',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 
 
@@ -93,13 +93,13 @@ L2 = [(4244,-0.5),(4244,-0.4),(5632,-0.4),(5632,-0.5),(0,0)]
 path = Path(L2, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((4244+5632)/2.0,-0.45,'L2',horizontalalignment='center',verticalalignment='center')
+ax.text((4244+5632)/2.0,-0.45,'L2',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 L1 = [(5430,-0.3),(5430,-0.2),(7136,-0.2),(7136,-0.3),(0,0)]
 path = Path(L1, codes)
 patch = patches.PathPatch(path, facecolor='green', lw=2)
 ax.add_patch(patch)
-ax.text((5430+7136)/2.0,-0.25,'L1',horizontalalignment='center',verticalalignment='center')
+ax.text((5430+7136)/2.0,-0.25,'L1',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 
 ax.axes.get_yaxis().set_visible(False)
