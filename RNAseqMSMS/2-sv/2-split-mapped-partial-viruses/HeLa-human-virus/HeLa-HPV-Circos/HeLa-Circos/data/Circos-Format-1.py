44ae706ef30bc8a522e7d93ca04916a4d9eaf5f7
hanice <sunahnice@gmail.com>
2013-05-01 09:30:26 +0800

    crontab.20130501

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-HPV-Circos/HeLa-Circos/data/Circos-Format-1.py
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-HPV-Circos/HeLa-Circos/data/Circos-Format-1.py
@@ -1,11 +1,16 @@
 def Circos_Format(inF):
+    D = {}
     inFile = open(inF)
     ouFile = open(inF+'.circos','w')
     for line in inFile:
         line = line.strip()
         fields = line.split('\t')
-        ouFile.write(fields[0]+'\t'+fields[1]+'\t'+str(int(fields[1])+1)+'\t'+str(0.2)+'\n')
+        p = fields[0]+'\t'+fields[1]
+        D.setdefault(p,1)
+        ouFile.write(fields[0]+'\t'+fields[1]+'\t'+str(int(fields[1])+1)+'\t'+str(D[p])+'\n')
+        D[p]-=0.02
     inFile.close()
     ouFile.close()
 
 Circos_Format('ERR0498-04-05.unmapped.unique.human-viruse-checked-human-chr-site')
+Circos_Format('ERR0498-04-05.unmapped.unique.human-viruse-checked-virus-site')
