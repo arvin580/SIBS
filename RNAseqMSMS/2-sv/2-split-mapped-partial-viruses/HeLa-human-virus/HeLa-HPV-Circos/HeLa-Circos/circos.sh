0eec188076b9d3ffb96a8a81e002867b75e01d25
hanice <sunahnice@gmail.com>
2013-05-01 21:30:27 +0800

    crontab.20130501

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-HPV-Circos/HeLa-Circos/circos.sh
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-HPV-Circos/HeLa-Circos/circos.sh
@@ -1,5 +1,5 @@
 cd /netshare1/home1/people/hansun/RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-HPV-Circos/HeLa-Circos 
-cd Human
+#cd Human
+#circos -svg
+cd HPV18
 circos -svg
-#cd HPV18
-#circos
