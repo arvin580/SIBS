e383fed6153f2015c360c815d4052d067dd6f40a
hanice <sunahnice@gmail.com>
2013-05-03 08:00:26 +0800

    crontab.20130503

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/examle-WRN/6-select-chr-region.py
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/examle-WRN/6-select-chr-region.py
@@ -1,6 +1,9 @@
-def select_chr_region(inF,ch,start,end):
+def select_chr_region(inF,ch,start,end,gene=''):
     inFile = open(inF)
-    ouFile = open(inF+'-'+ch+'-'+str(start)+'-'+str(end),'w')
+    if gene:
+        ouFile = open(inF+'-'+gene+'-'+ch+'-'+str(start)+'-'+str(end),'w')
+    else:
+        ouFile = open(inF+'-'+ch+'-'+str(start)+'-'+str(end),'w')
     while True:
         line1 = inFile.readline().strip()
         line2 = inFile.readline().strip()
@@ -34,8 +37,17 @@ def select_chr_region(inF,ch,start,end):
 #select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','NC_001357.1',1420,1428)
 #select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','NC_001357.1',2236,2244)
 #select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','NC_001357.1',439,447)
-select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr11',108075402,108286638 )
-select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr16',69304112,69519874)
-select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr11',61462834,61663976)
-select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr22',41917295,42160052)
-select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr8',30841215,31048048)
+#select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr11',108075402,108286638 )
+#select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr16',69304112,69519874)
+#select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr11',61462834,61663976)
+#select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr22',41917295,42160052)
+#select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr8',30841215,31048048)
+
+select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr1',43824779-100000,43828873+100000,'CDC20')
+select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr15',40886447-100000,40917838+100000,'CASC5')
+select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr4',120980579-100000,120988013+100000,'MAD2L1')
+select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr17',45199810-100000,45266665+100000,'CDC27')
+select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr4',104059508-100000,104061571+100000,'CENPE')
+select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr9',140081125-100000,140083057+100000,'ANAPC2')
+select_chr_region('ERR0498-04-05.unmapped.unique.human-viruse-checked','chr13',115000362-100000,115008823+100000,'CDC16')
+
