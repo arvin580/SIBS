8d7c1e1f7fbb5756dbbffc5229c4ee7c51b24990
hanice <sunahnice@gmail.com>
2013-05-04 22:30:26 +0800

    crontab.20130504

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-translation-truncated/3-reads-msms.py
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-translation-truncated/3-reads-msms.py
@@ -1,50 +1,45 @@
-#inFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked-random-10000')
-inFile = open('ha')
-ouFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked-random-10000-msms','w')
+ouFile = open('HeLa-msms-identified-reads','w')
+D2 = {}
+inFile = open('HeLa-Known-Protein')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    D = fields
+    for item in fields:
+        its = item.split(':')
+        for it in its:
+            if it[0:3]=='ERR' and it.find('.')!=-1:
+                D2.setdefault(it,[])
+                D2[it].append(line)
+inFile.close()
 
-    D2 = {}
-    inFile = open('HeLa-Known-Protein')
-    for line in inFile:
-        line = line.strip()
-        fields = line.split('\t')
-        for item in fields:
-            its = item.split(':')
-            for it in its:
-                if it in D:
-                    D2.setdefault(it,[])
-                    D2[it].append(line)
-    inFile.close()
-    
-    inFile = open('HeLa-Human-Viruses')
-    for line in inFile:
-        line = line.strip()
-        fields = line.split('\t')
-        for item in fields:
-            its = item.split(':')
-            for it in its:
-                if it in D:
-                    D2.setdefault(it,[])
-                    D2[it].append(line)
-    inFile.close()
-    
-    inFile = open('HeLa-Viruses')
-    for line in inFile:
-        line = line.strip()
-        fields = line.split('\t')
-        for item in fields:
-            its = item.split(':')
-            for it in its:
-                if it in D:
-                    D2.setdefault(it,[])
-                    D2[it].append(line)
-    inFile.close()
-    
-    
-    
-    for k in D2:
-        ouFile.write(str(len(D2))+'\n')
+inFile = open('HeLa-Human-Viruses')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    for item in fields:
+        its = item.split(':')
+        for it in its:
+            if it[0:3]=='ERR' and it.find('.')!=-1:
+                D2.setdefault(it,[])
+                D2[it].append(line)
+inFile.close()
+
+inFile = open('HeLa-Viruses')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    for item in fields:
+        its = item.split(':')
+        for it in its:
+            if it[0:3]=='ERR' and it.find('.')!=-1:
+                D2.setdefault(it,[])
+                D2[it].append(line)
+inFile.close()
+
+
+
+for k in D2:
+    ouFile.write(k+'\t'+'|'.join(D2[k])+'\n')
+
+ouFile.close()
 
