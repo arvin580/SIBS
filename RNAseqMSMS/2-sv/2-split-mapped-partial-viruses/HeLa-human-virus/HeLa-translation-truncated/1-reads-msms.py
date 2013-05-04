e9431cbbc9f113d694a4c8b6f257435ff93cd996
hanice <sunahnice@gmail.com>
2013-05-04 18:30:29 +0800

    crontab.20130504

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-translation-truncated/1-reads-msms.py
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-translation-truncated/1-reads-msms.py
@@ -10,6 +10,8 @@ while True:
         break
 inFile.close()
 
+ouFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked-msms','w')
+
 D2 = {}
 inFile = open('HeLa-Known-Protein')
 for line in inFile:
@@ -23,7 +25,32 @@ for line in inFile:
                 D2[it].append(line)
 inFile.close()
 
+inFile = open('HeLa-Human-Viruses')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    for item in fields:
+        its = item.split(':')
+        for it in its:
+            if it in D:
+                D2.setdefault(it,[])
+                D2[it].append(line)
+inFile.close()
+
+inFile = open('HeLa-Viruses')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    for item in fields:
+        its = item.split(':')
+        for it in its:
+            if it in D:
+                D2.setdefault(it,[])
+                D2[it].append(line)
+inFile.close()
+
+
+
 for k in D2:
-    print(k)
-    print(D2[k])
+    ouFile.write(k+'\t'+'\t'.join(D2[k])+'\n')
 
