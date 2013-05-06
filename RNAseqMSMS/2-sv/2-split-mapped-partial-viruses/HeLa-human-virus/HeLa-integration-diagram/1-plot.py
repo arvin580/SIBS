98c586794452e252945dd962a73351f1e4c06707
hanice <sunahnice@gmail.com>
2013-05-06 18:00:26 +0800

    crontab.20130506

--- RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-integration-diagram/1-plot.py
+++ RNAseqMSMS/2-sv/2-split-mapped-partial-viruses/HeLa-human-virus/HeLa-integration-diagram/1-plot.py
@@ -1,13 +1,39 @@
-D = {}
+import string
+Read = {}
 inFile = open('/netshare1/home1/people/hansun/RNAseqMSMS/2-sv/ERR0498-04-05.unmapped.unique.fasta')
 while True:
     line1 = inFile.readline().strip('>\n')
     line2 = inFile.readline().strip()
     if line1:
-        D[line1] = line2
+        Read[line1] = line2
     else:
         break
 inFile.close()
+
+Human = {}
+inFile = open('/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta.fa')
+while True:
+    line1 = inFile.readline().strip('>\n')
+    line2 = inFile.readline().strip()
+    if line1:
+        Human[line1] = line2
+    else:
+        break
+inFile.close()
+
+HPV = {}
+inFile = open('/netshare1/home1/people/hansun/RNAseqMSMS/6-tandem-viruses/HPV-genome.fa')
+while True:
+    line1 = inFile.readline().strip('>\n')
+    line2 = inFile.readline().strip()
+    if line1:
+        HPV[line1.split()[0]] = line2
+    else:
+        break
+inFile.close()
+
+trans = string.maketrans('ATCGatcg','TAGCtagc')
+
 
 inFile = open('ha')
 for line in inFile:
@@ -24,7 +50,32 @@ for line in inFile:
     pos3_subject = int(fields[22])
     pos4_subject = int(fields[23])
     read = fields[0]
-    print(D[read])
-
+    print(Read[read])
+    if ch1 == 'NC_001357.1':
+        if pos3_subject < pos4_subject:
+            seq=Human[ch2][pos3_subject-1:pos4_subject]
+            print(seq)
+        else:
+            seq = string.translate(Human[ch2][pos4_subject-1:pos3_subject][::-1],trans)
+            print(seq)
+        if pos1_subject < pos2_subject:
+            seq=HPV[ch1][pos1_subject-1:pos2_subject]
+            print(seq)
+        else:
+            seq = string.translate(Human[ch1][pos2_subject-1:pos1_subject][::-1],trans)
+            print(seq)
+    else:
+        if pos1_subject < pos2_subject:
+            seq = Human[ch1][pos1_subject-1:pos2_subject]
+            print(seq)
+        else:
+            seq = string.translate(Human[ch1][pos2_subject-1:pos1_subject][::-1],trans)
+            print(seq)
+        if pos3_subject < pos4_subject:
+            seq =HPV[ch2][pos3_subject-1:pos4_subject]
+            print(seq)
+        else:
+            seq = string.translate(Human[ch2][pos4_subject-1:pos3_subject][::-1],trans)
+            print(seq)
 
 inFile.close()
