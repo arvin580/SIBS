066cbfc78ef8093674ce8b6ac9addc87b9fdbd0a
hanice <sunahnice@gmail.com>
2013-05-14 02:30:27 +0800

    crontab.20130514

---
+++ Data/ucsc/reverse-unique.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+
+D = {}
+def unique(inF,flag=''):
+    inFile = open(inF)
+    while True:
+        line1 = inFile.readline().strip('>\n')
+        line2 = inFile.readline().strip()
+        if line1:
+            D.setdefault(line2,[])
+            if flag:
+                D[line2].append(flag+':'+':'.join(line1.split('\t')))
+            else:
+                D[line2].append(':'.join(line1.split('\t')))
+        else:
+            break
+    inFile.close()
+
+unique(sys.argv[1])
+
+ouFile = open(sys.argv[1].split('.fa')[0]+'.reversed.fa','w')
+for k in D:
+    ouFile.write('>'+'|'.join(D[k])+'\n')
+    ouFile.write(k+'\n')
+    ouFile.write('>REVERSE:'+'|'.join(D[k])+'\n')
+    ouFile.write(k[::-1]+'\n')
+
+ouFile.close()
+
