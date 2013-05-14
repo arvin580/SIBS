23176b8b1b19358a264284bc3448122170bd15e7
hanice <sunahnice@gmail.com>
2013-05-14 17:30:27 +0800

    crontab.20130514

--- RNAseqMSMS/2-sv/2-split-mapped-splicing/10-blat-filter.py
+++ RNAseqMSMS/2-sv/2-split-mapped-splicing/10-blat-filter.py
@@ -1,16 +1,20 @@
 import os
-D = {}
-ouFile = open('ERR0498-04-05.unmapped.unique.total.fasta.blated.filtered.seq1.not-splicing.fa.blated-95-70','w')
-files =  os.listdir('.')
-for f in files:
-    if f[-7:] == '.blated':
-        inFile = open(f)
-        for line in inFile:
-            line = line.strip()
-            fields = line.split('\t')
-            if float(fields[2]) >= 95  and int(fields[7]) -int(fields[6]) >= 70:
-                D[fields[0]] = line
-        inFile.close()
+def filter(n1,n2):
+    D = {}
+    ouFile = open('ERR0498-04-05.unmapped.unique.total.fasta.blated.filtered.seq1.not-splicing.fa.blated-%s-%s'%(n1,n2),'w')
+    files =  os.listdir('.')
+    for f in files:
+        if f[-7:] == '.blated':
+            inFile = open(f)
+            for line in inFile:
+                line = line.strip()
+                fields = line.split('\t')
+                if float(fields[2]) >= n1  and int(fields[7]) -int(fields[6]) >= n2:
+                    D[fields[0]] = line
+            inFile.close()
+    
+    for k in D:
+        ouFile.write(D[k]+'\n')
 
-for k in D:
-    ouFile.write(D[k]+'\n')
+filter(95,70)
+filter(100,75)
