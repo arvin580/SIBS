de694827e8ab8b11230df010b80afbfa2de278ce
hanice <sunahnice@gmail.com>
2013-05-15 22:30:30 +0800

    crontab.20130515

--- Data/GenomeSeq/Human/find.py
+++ Data/GenomeSeq/Human/find.py
@@ -1,16 +1,24 @@
 import  sys
-import re
-inFile=open('ucsc.hg19.fasta.fa')
-
-file=inFile.read()
-
+import string
+D = {}
+inFile = open('ucsc.hg19.fasta.fa')
+while True:
+    line1 = inFile.readline().strip()
+    line2 = inFile.readline().strip().upper()
+    if line1:
+        D[line1] = line2
+    else:
+        break
 inFile.close()
-
-s=re.findall(sys.argv[1],file,re.I)
-
-print(s)
-
-
-
-
+trans = string.maketrans('ATCG','TAGC')
+for k in D:
+    pos = D[k].find(sys.argv[1].upper())
+    if pos != -1:
+        print(k)
+        print(pos)
+    rev = string.translate(sys.argv[1].upper()[::-1],trans)
+    pos = D[k].find(rev)
+    if pos != -1:
+        print(k+':'+'REVERSE')
+        print(pos)
 
