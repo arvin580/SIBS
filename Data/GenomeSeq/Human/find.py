2588f9ba45b78a4b9209f9b844e505e0ea67f6d8
hanice <sunahnice@gmail.com>
2013-05-15 23:00:29 +0800

    crontab.20130515

--- Data/GenomeSeq/Human/find.py
+++ Data/GenomeSeq/Human/find.py
@@ -1,24 +1,28 @@
 import  sys
 import string
+import re
+chrs = ['>chr1','>chr2','>chr3','>chr4','>chr5','>chr6','>chr7','>chr8','>chr9','>chr10','>chr11','>chr12','>chr13','>chr14','>chr15','>chr16','>chr17','>chr18','>chr19','>chr20','>chr21','>chr22','>chrX','>chrY','>chrM']
 D = {}
 inFile = open('ucsc.hg19.fasta.fa')
+ouFile = open('ucsc.hg19.fasta.fa'+'-'+''.join(sys.argv[1].split()),'w')
 while True:
     line1 = inFile.readline().strip()
     line2 = inFile.readline().strip().upper()
-    if line1:
+    if line1 and line1 in chrs:
         D[line1] = line2
     else:
         break
 inFile.close()
 trans = string.maketrans('ATCG','TAGC')
 for k in D:
-    pos = D[k].find(sys.argv[1].upper())
-    if pos != -1:
-        print(k)
-        print(pos)
-    rev = string.translate(sys.argv[1].upper()[::-1],trans)
-    pos = D[k].find(rev)
-    if pos != -1:
-        print(k+':'+'REVERSE')
-        print(pos)
-
+    #pos = D[k].find(sys.argv[1].upper())
+    #if pos != -1:
+    #    print(k)
+    #    print(pos)
+    query = sys.argv[1].upper()
+    for item in re.finditer(query,D[k]):
+        ouFile.write(k+'\t'+str(item.start())+'\t'+item.group()+'\n')
+    rev = string.translate(query[::-1],trans)
+    for item in re.finditer(rev,D[k]):
+        ouFile.write(k+'\t'+str(item.start())+'\t'+item.group()+'\n')
+ouFile.close()
