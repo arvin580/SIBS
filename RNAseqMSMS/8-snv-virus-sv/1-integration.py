a69bddd9406c8c3379169eeb94619d7d1c7b1576
hanice <sunahnice@gmail.com>
2013-05-16 18:00:31 +0800

    crontab.20130516

--- RNAseqMSMS/8-snv-virus-sv/1-integration.py
+++ RNAseqMSMS/8-snv-virus-sv/1-integration.py
@@ -1,2 +1,19 @@
-inFile = open()
+### [SNV, Virus, Deltion, Duplication, Inversion, Translocation]
+inFile = open('sum_snv.exome_summary.overall.filter')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    gene = fields[1]
+    D.setdefault(gene,[0,0,0,0,0,0])
+    D[gene][0] = 1
+inFile.close()
+
+
+inFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked-human-gene3')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    gene = fields[0]
+    D.setdefault(gene,[0,0,0,0,0,0])
+    D[gene][1] = 1
 inFile.close()
