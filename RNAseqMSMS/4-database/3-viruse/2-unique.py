8b0bebd9b40086ec481321adb1e52ba17869d6bd
hanice <sunahnice@gmail.com>
2013-05-17 00:00:41 +0800

    crontab.20130517

---
+++ RNAseqMSMS/4-database/3-viruse/2-unique.py
@@ -0,0 +1,31 @@
+D = {}
+
+def unique(inF,flag=''):
+    inFile = open(inF)
+    while True:
+        line1 = inFile.readline().strip('>\n')
+        line2 = inFile.readline().strip()
+        if line1:
+            D.setdefault(line2,[])
+            line1 = ':'.join(line1.split('\t'))
+            if flag:
+                D[line2].append(flag+':'+line1)
+            else:
+                D[line2].append(line1)
+        else:
+            break
+    inFile.close()
+
+unique('Homo_sapiens.GRCh37.70.pep.all.fa.fa')
+unique('ERR0498-04-05.unmapped.unique.human-viruse.pep2','HUMAN-VIRUS')
+unique('unmapped-blated-viruses-90-60.seq.pep2','VIRUS')
+
+ouFile = open('Homo_known-protein_virus2','w')
+for k in D:
+    ouFile.write('>'+'|'.join(D[k])+'\n')
+    ouFile.write(k+'\n')
+    ouFile.write('>REVERSE:'+'|'.join(D[k])+'\n')
+    ouFile.write(k[::-1]+'\n')
+
+ouFile.close()
+
