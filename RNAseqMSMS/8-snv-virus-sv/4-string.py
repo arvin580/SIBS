ecb6e2bab57306175f4b8b68267495e97e3a8c24
hanice <sunahnice@gmail.com>
2013-05-16 22:00:31 +0800

    crontab.20130516

--- RNAseqMSMS/8-snv-virus-sv/4-string.py
+++ RNAseqMSMS/8-snv-virus-sv/4-string.py
@@ -8,19 +8,23 @@ for line in inFile:
 inFile.close()
 
 STRING = {}
+STRING2 = {}
 inFile = open('/netshare1/home1/people/hansun/Data/String/protein.links.v9.05.human.gene')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
     STRING.setdefault(fields[0],[])
+    STRING2.setdefault(fields[0],0)
     for item in fields:
         if item in D:
             STRING[fields[0]].append('('+item+')')
+            STRING2[fields[0]]+=1
+
         else:
             STRING[fields[0]].append(item)
 
 inFile.close()
 for k in STRING:
-    ouFile.write('\t'.join(STRING[k])+'\n')
+    ouFile.write(str(STRING2[k])+'\t'+str(len(STRING[k]))+'\t'+str(float(STRING2[k])/len(STRING[k]))+'\t'+'\t'.join(STRING[k])+'\n')
 
 ouFile.close()
