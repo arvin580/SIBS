ad0294f9eaace2f4430e66e99a776614aaa8b806
hanice <sunahnice@gmail.com>
2013-05-17 21:30:26 +0800

    crontab.20130517

---
+++ DayDayCoding/xtandem-tools/tandem-mgf-peptide.py
@@ -0,0 +1,30 @@
+import sys
+DIR = '/netshare1/home1/people/hansun/Data/HeLa/HeLa-MSMS'
+
+def spec(sp):
+    inFile = open(DIR+'/'+sp.split('.')[0]+'.mgf')
+    L = inFile.readlines()
+    inFile.close()
+    s = L.index('TITLE=%s\n'%sp)
+    e = s + L[s:].index('END IONS\n')
+    mgf = L[s-1:e+1]
+    #print(''.join(mgf))
+    return mgf
+
+def peptide(inF):
+    inFile = open(inF)
+    ouFile1 = open(inF+'-pep-spec.mgf','w')
+    ouFile2 = open(inF+'-pep-spec.note','w')
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        pep = fields[0]
+        sp = fields[1].split(':')[0]
+        mgf = spec(sp)
+        ouFile1.write(''.join(mgf))
+        ouFile2.write(pep + '\t' + sp+'\n')
+    inFile.close()
+    ouFile1.close()
+    ouFile2.close()
+
+peptide(sys.argv[1])
