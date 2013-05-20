d04f6ab5356b3e179ba628b5c6f8194271cecaab
hanice <sunahnice@gmail.com>
2013-05-21 00:30:28 +0800

    crontab.20130521

--- RNAseqMSMS/9-blast-diagram/1-diagram.py
+++ RNAseqMSMS/9-blast-diagram/1-diagram.py
@@ -1,3 +1,40 @@
+import string
+
+def seq(ch,start,end):
+    trans = string.maketrans('ATCGatcg','TAGCtagc')
+    if ch[0:2] == 'ch':
+        inFile = open('/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta.fa')
+        while True:
+            line1 = inFile.readline().strip()
+            line2 = inFile.readline().strip()
+            if line1:
+                if line1 == '>'+ch:
+                    if start <= end:
+                        seq = line2[start-1:end].upper()
+                    else:
+                        seq = string.translate(line2[end-1:start][::-1],trans).upper()
+                    return seq
+            else:
+                break
+        inFile.close()
+    elif ch[0:2] == 'NC':
+        inFile = open('/netshare1/home1/people/hansun/Data/VirusesGenome/VirusesGenome.fasta.fa')
+        while True:
+            line1 = inFile.readline().strip()
+            line2 = inFile.readline().strip()
+            if line1:
+                if line1.find('>'+ch) == 0:
+                    if start <= end:
+                        seq = line2[start-1:end].upper()
+                    else:
+                        seq = string.translate(line2[end-1:start][::-1],trans).upper()
+                    return seq
+            else:
+                break
+        inFile.close()
+
+
+
 def diagram(inF):
     inFile = open(inF)
     L0 = [0]*76
@@ -24,6 +61,8 @@ def diagram(inF):
     
             seq1 = seq(ch1,start1, end1)
             seq2 = seq(ch2,start2, end2)
+            print(seq1)
+            print(seq2)
 
         else:
             break
