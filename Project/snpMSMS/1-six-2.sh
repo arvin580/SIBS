2e4c0108ad85f16293820e3b7e88fd9f0640ecba
hanice <sunahnice@gmail.com>
2013-05-24 23:30:32 +0800

    crontab.20130524

---
+++ Project/snpMSMS/1-six-2.sh
@@ -0,0 +1,2 @@
+cd /netshare1/home1/people/hansun/Project/snpMSMS 
+python six-frame-translation.py snp137CodingDbSnp-indel-formated
---
+++ Project/snpMSMS/1-six-4.sh
@@ -0,0 +1,2 @@
+cd /netshare1/home1/people/hansun/Project/snpMSMS 
+python six-frame-translation-cosmic.py CosmicCodingMuts_v64_02042013_noLimit-indel-formated
