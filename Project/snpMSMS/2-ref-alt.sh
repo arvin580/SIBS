9ea5a5b787797fa0e70c38da09dd6cda85e7c82c
hanice <sunahnice@gmail.com>
2013-05-26 17:00:32 +0800

    crontab.20130526

---
+++ Project/snpMSMS/2-ref-alt.sh
@@ -0,0 +1,2 @@
+cd /netshare1/home1/people/hansun/Project/snpMSMS 
+python ref-alt.py snp137CodingDbSnp-indel.formated.pep
---
+++ Project/snpMSMS/4-ref-alt.sh
@@ -0,0 +1,2 @@
+cd /netshare1/home1/people/hansun/Project/snpMSMS 
+python ref-alt.py CosmicCodingMuts_v64_02042013_noLimit-indel.formated.pep
