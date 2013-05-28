983de5cc08f8d93d0bbf32246a8da98221b0eb35
hanice <sunahnice@gmail.com>
2013-05-28 18:00:30 +0800

    crontab.20130528

---
+++ RNAseqMSMS/10-KR-pfind/1-cat.sh
@@ -0,0 +1,3 @@
+cat HeLa-SNV-INDEL-ALT-pep-spec.mgf HeLa-Human-Viruses-pep-spec.mgf HeLa-Predict-Splicing-pep-spec.mgf \
+	HeLa-SV-Deletion-pep-spec.mgf HeLa-SV-Duplication-pep-spec.mgf HeLa-SV-Inversion-pep-spec.mgf \
+       	HeLa-SV-Translocation-pep-spec.mgf  >HeLa-SNV-Virus-SV.mgf
