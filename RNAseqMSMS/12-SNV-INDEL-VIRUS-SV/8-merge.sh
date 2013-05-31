7d28e64e56e5da7ed01e00a6f4766407bc4c7c04
hanice <sunahnice@gmail.com>
2013-05-31 23:30:25 +0800

    crontab.20130531

--- RNAseqMSMS/12-SNV-INDEL-VIRUS-SV/8-merge.sh
+++ RNAseqMSMS/12-SNV-INDEL-VIRUS-SV/8-merge.sh
@@ -1,1 +1,1 @@
-cat Homo_sapiens.GRCh37.70.pep.all.fa.fa  HeLa-SNV-Virus-SV.fa >HeLa-known-SNV-Virus-SV.fa
+cat /netshare1/home1/people/hansun/Data/Ensembl/Homo_sapiens.GRCh37.70.pep.all.fa.fa  HeLa-SNV-INDEL-VIRUS-SV-MGF.fa >HeLa-KNOWN-SNV-INDEL-VIRUS-SV-MGF.fa
