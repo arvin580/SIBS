ef496b27b205e116629aa57fa942f705e89109e5
hanice <sunahnice@gmail.com>
2013-07-10 08:30:31 +0800

    crontab.20130710

--- RNAseqMSMS/18-snv-indel-sv-virus-KR-maxquant/4-gene.py
+++ RNAseqMSMS/18-snv-indel-sv-virus-KR-maxquant/4-gene.py
@@ -19,6 +19,18 @@ for line in inFile:
     L.append(fields)
 inFile.close()
 
+VIRUS_GENE={}
+VIRUS_GENE['E1']=[914,2887]
+VIRUS_GENE['E2']=[2817,3914]
+VIRUS_GENE['E4']=[3418,3684]
+VIRUS_GENE['E5']=[3936,4157]
+VIRUS_GENE['E6']=[105,581]
+VIRUS_GENE['E7']=[590,907]
+VIRUS_GENE['L1']=[5430,7136]
+VIRUS_GENE['L2']=[4244,5632]
+VIRUS_GENE['LCR']=[0,104]
+
+
 
 def snv_indel(line):
     fields = line.split('\t')
@@ -56,6 +68,87 @@ def predict(line):
             ouFile.write('|'.join(set(genes))+'\t'+''+'\t'+line+'\n')
             break
 
+def virus(line):
+    fields = line.split('\t')
+    for item in fields:
+        if item.find('VIRUS:')!=-1:
+            nc= item.split(':')[2]
+            pos1_nc = item.split(':')[9]
+            pos2_nc = item.split(':')[10]
+            pos5_nc_query = item.split(':')[7]
+            pos6_nc_query = item.split(':')[8]
+            genes = []
+            for k in VIRUS_GENE:
+                if VIRUS_GENE[k][0]<=int(pos1_nc)<=VIRUS_GENE[k][1] or VIRUS_GENE[k][0]<=int(pos2_nc)<=VIRUS_GENE[k][1]:
+                    genes.append(k)
+            ouFile.write('|'.join(set(genes))+'\t'+''+'\t'+line+'\n')
+            break
+
+def human_virus(line):
+    pass
+
+def sv_not_translocation(line):
+    fields = line.split('\t')
+    for item in fields:
+        if item.find('SV:')!=-1:
+            nc= item.split(':')[4]
+            ch = item.split(':')[16]
+            pos1_nc = item.split(':')[11]
+            pos2_nc = item.split(':')[12]
+            pos3_ch = item.split(':')[23]
+            pos4_ch = item.split(':')[24]
+        
+            pos5_nc_query = item.split(':')[9]
+            pos6_nc_query = item.split(':')[10]
+            pos7_ch_query = item.split(':')[21]
+            pos8_ch_query = item.split(':')[22]
+            genes = []
+            for item in L:
+                if (item[2]==ch and (int(item[4])<=int(pos3_ch)<=int(item[5]) or int(item[4])<=int(pos4_ch)<=int(item[5]))) or \
+                        (item[2]==nc and (int(item[4])<=int(pos1_nc)<=int(item[5]) or int(item[4])<=int(pos2_nc)<=int(item[5]))):
+                    gene = item[12]
+                    genes.append(gene)
+            ouFile.write('|'.join(set(genes))+'\t'+''+'\t'+line+'\n')
+            break
+
+
+
+def sv_translocation(line):
+    fields = line.split('\t')
+    for item in fields:
+        if item.find('SV:')!=-1:
+            nc= item.split(':')[4]
+            ch = item.split(':')[16]
+            pos1_nc = item.split(':')[11]
+            pos2_nc = item.split(':')[12]
+            pos3_ch = item.split(':')[23]
+            pos4_ch = item.split(':')[24]
+    
+            pos5_nc_query = item.split(':')[9]
+            pos6_nc_query = item.split(':')[10]
+            pos7_ch_query = item.split(':')[21]
+            pos8_ch_query = item.split(':')[22]
+            genes1 = []
+            genes2 = []
+            for item in L:
+                if (item[2]==nc and (int(item[4])<=int(pos1_nc)<=int(item[5]) or int(item[4])<=int(pos2_nc)<=int(item[5]))):
+                    genes1.append(item[12])
+                if (item[2]==ch and (int(item[4])<=int(pos3_ch)<=int(item[5]) or int(item[4])<=int(pos4_ch)<=int(item[5]))):
+                    genes2.append(item[12])
+            if genes1:
+                genes1 = '|'.join(set(genes1))
+            else:
+                genes1 = '*' 
+            if genes2:
+                genes2 = '|'.join(set(genes2))
+            else:
+                genes2 = '*' 
+            gene = genes1+':'+genes2
+            ouFile.write(gene+'\t'+''+'\t'+line+'\n')
+            break
+
+
+
 inFile = open('HeLa-peptide-snv-indel-predict-virus-sv')
 ouFile = open('HeLa-peptide-snv-indel-predict-virus-sv-gene','w')
 for line in inFile:
@@ -63,8 +156,17 @@ for line in inFile:
     fields = line.split('\t')
     if fields[1]=='SNV' or fields[1]=='INDEL':
         snv_indel(line)
-    elif fields[1] == 'PREDICT':
+    elif fields[1].find('PREDICT')!=-1:
         predict(line)
+    elif fields[1]=='VIRUS':
+        virus(line)
+    elif fields[1]=='HUMAN-VIRUS':
+        pass
+    elif fields[1]=='SV-DELETION' or fields[1]=='SV-DUPLICATION' or fields[1]=='SV-INVERSION':
+        sv_not_translocation(line)
+    elif fields[1]=='SV-TRANSLOCATION':
+        sv_translocation(line)
+
 
 inFile.close()
 ouFile.close()
