3a6f4ff76ab46e7d064ea00e293fbae4ebb9b3f2
hanice <sunahnice@gmail.com>
2013-07-14 08:00:29 +0800

    crontab.20130714

---
+++ RNAseqMSMS/18-snv-indel-predict-sv-virus-maxquant/9-quality.sh
@@ -0,0 +1,1 @@
+grep Normal-Not-Matched  HeLa-variant-Trypsin-LysC-GluC-evidence-unique-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_minus-spec|grep NEW |grep no-miss-cleavage >ha
