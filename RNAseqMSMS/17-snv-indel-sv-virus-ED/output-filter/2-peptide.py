9eb6f3dae4ea783a99dbecf4548c93b98baa9279
hanice <sunahnice@gmail.com>
2013-08-04 07:30:37 +0800

    crontab.20130804

---
+++ RNAseqMSMS/17-snv-indel-sv-virus-ED/output-filter/2-peptide.py
@@ -0,0 +1,41 @@
+inFile = open('HeLa-SNV-INDEL-ALT')
+
+D = {}
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+
+    start = int(fields[4].split(':')[0])
+    end = int(fields[4].split(':')[1])
+    e = fields[4].split(':')[2]
+    hyper = fields[4].split(':')[3]
+    pre = fields[4].split(':')[5]
+    post = fields[4].split(':')[6]
+    miss = fields[4].split(':')[7]
+    modi = []
+    try:
+        modi = fields[5].split(':')
+        for i in range(1,len(modi),3):
+            modi[i] = str(int(modi[i]) - start + 1)
+    except:
+        pass
+    modi = ':'.join(modi)
+    #s = int(fields[1].split(':')[-3])
+    #e = int(fields[1].split(':')[-2])
+    D.setdefault(fields[3], []) 
+    #D[fields[3]].append(fields[1]+':'+str(s-start+1)+':'+str(e-start+1)+':'+pre+':'+post)
+    D[fields[3]].append(fields[0].strip()+'|'+fields[1]+'|'+e+'|'+hyper+'|'+miss+'|'+pre+'|'+post+'|'+modi)
+
+inFile.close()
+
+ouFile = open('HeLa-SNV-INDEL-ALT-pep','w')
+for k in D:
+    v = '\t'.join(D[k])
+    if v.find('SNV')!=-1:
+        ouFile.write(k+'\t'+v+'\n')
+for k in D:
+    v = '\t'.join(D[k])
+    if v.find('INDEL')!=-1:
+        ouFile.write(k+'\t'+v+'\n')
+
+ouFile.close()
---
+++ RNAseqMSMS/4-database/6-snv-indel-sv-virus-ED/1-unique-reverse-KR.py
@@ -0,0 +1,38 @@
+D = {}
+
+def unique(inF,flag=''):
+    inFile = open(inF)
+    while True:
+        line1 = inFile.readline().strip('>\n')
+        line2 = inFile.readline().strip()
+        if line1:
+            D.setdefault(line2,[])
+            line1 = ':'.join(line1.split('\t'))
+            if flag:
+                D[line2].append(flag+':'+line1)
+            else:
+                D[line2].append(line1)
+        else:
+            break
+    inFile.close()
+
+unique('Homo_sapiens.GRCh37.70.pep.all.fa.fa')
+unique('Homo_sapiens.GRCh37.70.pep.abinitio.fa.fa')
+unique('sum_snv.exome_summary.nonsynonymous-splicing.pep.ref-alt.nonstop-ED','SNV')
+unique('sum_snv.exome_summary.nonsynonymous-splicing.pep.not-ref-alt.nonstop-ED','SNV')
+unique('sum_snv.exome_summary.indel.pep.ref-alt.nonstop-ED','INDEL')
+unique('sum_snv.exome_summary.indel.pep.not-ref-alt.nonstop-ED','INDEL')
+unique('split-mapped-deletion-translocation-inversion-duplication-ED','SV')
+unique('ERR0498-04-05.unmapped.unique.total.fasta.blated.filtered.seq1.splicing.not_known-predict-ED','PREDICT-SPLICING')
+unique('ERR0498-04-05.unmapped.unique.human-viruse.pep-ED','HUMAN-VIRUS')
+unique('unmapped-blated-viruses-90-60.seq.pep-ED','VIRUS')
+
+ouFile = open('HeLa_known-predicted-snv-indel-sv-virus-reverse-ED.fasta','w')
+for k in D:
+    ouFile.write('>'+'|'.join(D[k])+'\n')
+    ouFile.write(k+'\n')
+    ouFile.write('>REVERSE:'+'|'.join(D[k])+'\n')
+    ouFile.write(k[::-1]+'\n')
+
+ouFile.close()
+
