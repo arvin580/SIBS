0715e4bf90ca06c9634f735f0b446d2e4868b1b5
hanice <sunahnice@gmail.com>
2013-08-08 00:30:30 +0800

    crontab.20130808

--- RNAseqMSMS/16-snv-indel-sv-virus-K/output-filter/2-peptide.py
+++ RNAseqMSMS/16-snv-indel-sv-virus-K/output-filter/2-peptide.py
@@ -9,7 +9,7 @@ for line in inFile:
     D[pep].append(line)
 inFile.close()
 
-ouFile = open('HeLa-peptide-snv-indel-predict-sv-virus-trypsin-pep','w')
+ouFile = open('HeLa-peptide-snv-indel-predict-sv-virus-lysc-pep','w')
 for k in D:
     fields = D[k][0].split('\t')
     ouFile.write(fields[0]+'\t'+fields[1]+'\t'+k+'\t'+str(len(D[k]))+'\t'+'\t'.join(D[k])+'\n')
