8f98bd22b9bd3fbc62f0a14b8c83f35a56978f32
hanice <sunahnice@gmail.com>
2013-08-08 23:31:29 +0800

    crontab.20130808

---
+++ RNAseqMSMS/19-snv-indel-predict-sv-virus-xtandem/13-xtandem-maxquant/2-cmp.py
@@ -0,0 +1,23 @@
+D = {}
+inFile = open('HeLa-variant-Trypsin-LysC-GluC-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_check-spec-to_validation_predict')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    pep = fields[6]
+    D[pep] = 1
+inFile.close()
+
+n = 0
+N = 0
+inFile = open('HeLa-variant-Trypsin-LysC-GluC-evidence-unique-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_minus-spec-to_validation_predict-sequest')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    pep = fields[7]
+    N += 1
+    if pep in D:
+        n += 1
+inFile.close()
+print(n)
+print(N)
+
