618c26fafbc4139b78dc9f84d9a5f9fdf2cd097a
hanice <sunahnice@gmail.com>
2013-08-09 22:30:42 +0800

    crontab.20130809

--- RNAseqMSMS/19-snv-indel-predict-sv-virus-xtandem/13-xtandem-maxquant/1-cmp.py
+++ RNAseqMSMS/19-snv-indel-predict-sv-virus-xtandem/13-xtandem-maxquant/1-cmp.py
@@ -1,23 +1,36 @@
 D = {}
-inFile = open('HeLa-variant-Trypsin-LysC-GluC-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_check-spec-to_validation_not_predict')
+D2 = {}
+inFile = open('HeLa-variant-Trypsin-LysC-GluC-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_check-spec-to_validation_not_predict-pFind3')
 for line in inFile:
-    line = line.strip()
+    line = line.rstrip()
     fields = line.split('\t')
-    pep = fields[6]
-    D[pep] = 1
+    pep = fields[9]
+    if fields[0] == 'pFind':
+        D.setdefault(pep,[])
+        D[pep].append(line) 
+    D2.setdefault(pep,[])
+    D2[pep].append(line) 
 inFile.close()
+print(len(D))
+print(len(D2))
+
 
 n = 0
 N = 0
 inFile = open('HeLa-variant-Trypsin-LysC-GluC-evidence-unique-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_minus-spec-to_validation_not_predict-sequest')
+ouFile = open('HeLa-variant-Trypsin-LysC-GluC-evidence-unique-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_minus-spec-to_validation_not_predict-sequest-pfind','w')
 for line in inFile:
-    line = line.strip()
+    line = line.rstrip()
     fields = line.split('\t')
     pep = fields[7]
     N += 1
     if pep in D:
         n += 1
+        ouFile.write('pFind'+'\t'+line+'\n')
+    else:
+        ouFile.write(''+'\t'+line+'\n')
 inFile.close()
+
 print(n)
 print(N)
 
---
+++ RNAseqMSMS/19-snv-indel-predict-sv-virus-xtandem/15-pfind-1.py
@@ -0,0 +1,31 @@
+D = {}
+inFile = open('12-pfind/gluc-0.2013_08_09_11_29_02_qry.peptides-pep.pep')
+for line in inFile:
+    line = line.strip()
+    D[line] = 1
+inFile.close()
+inFile = open('12-pfind/trypsin-0.2013_08_09_11_29_00_qry.peptides-pep.pep')
+for line in inFile:
+    line = line.strip()
+    D[line] = 1
+inFile.close()
+inFile = open('12-pfind/lysc-0.2013_08_09_11_29_01_qry.peptides-pep.pep')
+for line in inFile:
+    line = line.strip()
+    D[line] = 1
+inFile.close()
+
+def pep(inF):
+    inFile = open(inF)
+    ouFile = open(inF+'-pFind1','w')
+    for line in inFile:
+        fields = line.split('\t')
+        if fields[6] in D:
+            ouFile.write('pFind-ALL-UNIQUE'+'\t'+line)
+        else:
+            ouFile.write(''+'\t'+line)
+    inFile.close()
+    ouFile.close()
+
+pep('HeLa-variant-Trypsin-LysC-GluC-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_check-spec-to_validation_predict')
+
