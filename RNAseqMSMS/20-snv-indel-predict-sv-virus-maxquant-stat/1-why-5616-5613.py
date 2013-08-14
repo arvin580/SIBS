0869a197396624c5fbd901a23638b592bb996f0d
hanice <sunahnice@gmail.com>
2013-08-14 20:00:40 +0800

    crontab.20130814

--- RNAseqMSMS/20-snv-indel-predict-sv-virus-maxquant-stat/1-why-5616-5613.py
+++ RNAseqMSMS/20-snv-indel-predict-sv-virus-maxquant-stat/1-why-5616-5613.py
@@ -6,3 +6,12 @@ for line in inFile:
     D[fields[3]] = 1
 inFile.close()
 print(len(D))
+
+inFile = open('HeLa-variant-Trypsin-LysC-GluC-evidence-unique-peptide-snv-indel-predict-sv-virus')
+for line in inFile:
+    line = line.rstrip()
+    fields = line.split('\t')
+    if fields[1] not in D:
+        print(line)
+inFile.close()
+
