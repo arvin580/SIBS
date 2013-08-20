8f6350baa0aae9a0e4cc3aa4e1414a783f63cd9c
hanice <sunahnice@gmail.com>
2013-08-20 08:30:28 +0800

    crontab.20130820

--- RNAseqMSMS/21-rna-seq-stats/5-plot.py
+++ RNAseqMSMS/21-rna-seq-stats/5-plot.py
@@ -25,16 +25,55 @@ print(D1['Translocation'])
 print(D2['Translocation'])
 print(D3['Translocation'])
 
+'''
+ymin = min(D3['Deletion']+D3['Duplication']+D3['Inversion']+D3['Translocation'])-1
+ymax = max(D3['Deletion']+D3['Duplication']+D3['Inversion']+D3['Translocation'])
+xmin = min(D1['Deletion']+D1['Duplication']+D1['Inversion']+D1['Translocation'])-1
+xmax = max(D1['Deletion']+D1['Duplication']+D1['Inversion']+D1['Translocation'])
+'''
+ymin = min(D3['Duplication']+D3['Inversion']+D3['Translocation'])-1
+ymax = max(D3['Duplication']+D3['Inversion']+D3['Translocation'])+1
+xmin = min(D1['Duplication']+D1['Inversion']+D1['Translocation'])-1
+xmax = max(D1['Duplication']+D1['Inversion']+D1['Translocation'])+1
+
+
+
+
 fig = plt.figure()
-ax=fig.add_subplot(111)
+ax=fig.add_subplot(211)
 
-xList=[D1['Translocation']]
-yList=[D3['Translocation']]
+ax.set_xlim(xmin,xmax)
+ax.set_ylim(ymin,ymax)
+'''
+xList=D1['Deletion']
+yList=D3['Deletion']
 ax.plot(xList,yList,linestyle='-', marker='*')
+'''
 
-xList=[D1['Inversion']]
-yList=[D3['Inversion']]
-ax.plot(xList,yList,linestyle='-', marker='o')
+xList=D1['Duplication']
+yList=D3['Duplication']
+ax.plot(xList,yList,linestyle='-', marker='o',color='blue')
+
+xList=D1['Inversion']
+yList=D3['Inversion']
+ax.plot(xList,yList,linestyle='-', marker='s',color='green')
+
+xList=D1['Translocation']
+yList=D3['Translocation']
+ax.plot(xList,yList,linestyle='-', marker='^',color='red')
+
+ymin = min(D3['Deletion']+D3['Duplication']+D3['Inversion']+D3['Translocation'])-1
+ymax = max(D3['Deletion']+D3['Duplication']+D3['Inversion']+D3['Translocation'])+1
+xmin = min(D1['Deletion']+D1['Duplication']+D1['Inversion']+D1['Translocation'])-1
+xmax = max(D1['Deletion']+D1['Duplication']+D1['Inversion']+D1['Translocation'])+1
+
+ax=fig.add_subplot(212)
+ax.set_xlim(xmin,xmax)
+ax.set_ylim(ymin,ymax)
+xList=D1['Deletion']
+yList=D3['Deletion']
+ax.plot(xList,yList,linestyle='-', marker='D',color='magenta',markeredgecolor='magenta')
+
 
 plt.savefig('haha.pdf')
 
