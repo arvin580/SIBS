208b3d31b31c56dd89c4db0e422b305b586ee83a
hanice <sunahnice@gmail.com>
2013-08-21 09:00:18 +0800

    crontab.20130821

--- RNAseqMSMS/21-rna-seq-stats/13-set.py
+++ RNAseqMSMS/21-rna-seq-stats/13-set.py
@@ -1,11 +1,5 @@
-from Rscript.RscriptClass import Rscript
-
-def L2c(L):
-    return 'c("'+ '","'.join(L) + '")'
-
 Name = []
 Set = []
-
 def read_data(inF,name):
     Name.append(name)
     L = []
@@ -15,21 +9,14 @@ def read_data(inF,name):
         fields = line.split('\t')
         L.append(fields[0])
     inFile.close()
-    Set.append(L)
+    Set.append(set(L))
 
 read_data('split-mapped-deletion.normal.seq.filtered.num.gene.more_than_one.gene','Deletion')
 read_data('split-mapped-duplication.normal.seq.filtered.num.gene.more_than_one.gene','Duplication')
 read_data('split-mapped-inversion.normal.seq.filtered.num.gene.more_than_one.gene','Inversion')
 read_data('split-mapped-translocation.normal.seq.filtered.num.gene.more_than_one.gene','Translocation')
 
+S = Set[0]& Set[1] & Set[2] & Set[3]
+for x in S:
+    print(x)
 
-Rcode=r'''
-library('Vennerable');
-L = list("%s"=%s, "%s"=%s, "%s"=%s, "%s"=%s);
-V = Venn(L);
-pdf("%s");
-plot(V,doWeights = FALSE);
-dev.off()
-'''%(Name[0],L2c(Set[0]),Name[1],L2c(Set[1]),Name[2],L2c(Set[2]),Name[3],L2c(Set[3]),'HeLa-SV-Gene-Number.pdf')
-
-R=Rscript(Rcode)
