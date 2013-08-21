160de7147a0c1fea12eadeba9696bd96299ddf76
hanice <sunahnice@gmail.com>
2013-08-21 08:30:15 +0800

    crontab.20130821

--- RNAseqMSMS/21-rna-seq-stats/12-venn.py
+++ RNAseqMSMS/21-rna-seq-stats/12-venn.py
@@ -1,10 +1,33 @@
+def L2c(L):
+    return 'c("'+ '","'.join(L) + '")'
+
+Name = []
+Set = []
+
+def read_data(inF,name):
+    Name.append(name)
+    L = []
+    inFile = open(inF)
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        L.append(fields[0])
+    inFile.close()
+    Set.append(L)
+
+read_data('split-mapped-deletion.normal.seq.filtered.num.gene.more_than_one.gene','Deletion')
+read_data('split-mapped-duplication.normal.seq.filtered.num.gene.more_than_one.gene','Duplication')
+read_data('split-mapped-inversion.normal.seq.filtered.num.gene.more_than_one.gene','Inversion')
+read_data('split-mapped-translocation.normal.seq.filtered.num.gene.more_than_one.gene','Translocation')
+
+
 Rcode=r'''
 library('Vennerable');
-L = list("%s=%s, %s=%s, %s=%s, %s=%s);
-
-plot(v);
+L = list("%s"=%s, "%s"=%s, "%s"=%s, "%s"=%s);
+V = Venn(L);
+plot(V,doWeights = FALSE);
 dev.off()
-'''%('Deletion','c(1,2,3)','Duplication','c(2,3,4)','Inversion','c(2,5)','Translocation','c(2)')
-#R=Rscript(Rcode)
+'''%(Name[0],L2c(Set[0]),Name[1],L2c(Set[1]),Name[2],L2c(Set[2]),Name[3],L2c(Set[3]))
+#
+##R=Rscript(Rcode)
 print(Rcode)
-
