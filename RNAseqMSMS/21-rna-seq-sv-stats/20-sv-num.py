085fb404f5356171d342af8ff43799a90d698cdc
hanice <sunahnice@gmail.com>
2013-08-27 23:00:29 +0800

    crontab.20130827

--- RNAseqMSMS/21-rna-seq-sv-stats/20-sv-num.py
+++ RNAseqMSMS/21-rna-seq-sv-stats/20-sv-num.py
@@ -1,2 +1,15 @@
-inFile = open()
+inFile = open('HeLa-Deletion-Duplication-Inversion-Translocation-Gene-more_than_two')
+ouFile = open('HeLa-Deletion-Duplication-Inversion-Translocation-Gene-more_than_two-num','w')
+D = {}
+for line in inFile:
+    fields = line.split('\t')
+    gene = fields[0]
+    D.setdefault(gene, 0)
+    D[gene] += 1
 inFile.close()
+
+d = D.items()
+d.sort(cmp= lambda x,y:cmp(x[1],y[1]),reverse=True)
+for item in d:
+    ouFile.write(item[0]+'\t'+str(item[1])+'\n')
+ouFile.close()
