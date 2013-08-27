c49aab2dafd8d8d589a781c253c85af8ad69f898
hanice <sunahnice@gmail.com>
2013-08-28 06:00:29 +0800

    crontab.20130828

---
+++ Project/LianBaoFeng/varScan.p1j1.chr21.sh
@@ -0,0 +1,19 @@
+cd /netshare1/home1/people/hansun/Project/LianBaoFeng
+varscan=/netshare1/home1/people/hansun/MySoft/VarScan/VarScan.v2.3.3.jar
+hg19=/netshare1/home1/people/hansun/Project/LianBaoFeng/hg19.fa
+
+P1=/netshare1/home1/people/hansun/Project/LianBaoFeng/p1.rmdup.bam
+B1=/netshare1/home1/people/hansun/Project/LianBaoFeng/b1.rmdup.bam
+N1=/netshare1/home1/people/hansun/Project/LianBaoFeng/n1.rmdup.bam
+J1=/netshare1/home1/people/hansun/Project/LianBaoFeng/j1.rmdup.bam
+W1=/netshare1/home1/people/hansun/Project/LianBaoFeng/w1.rmdup.bam
+
+sample=P1J1
+chr=chr21
+
+samtools mpileup -q 1 -f $hg19 -r $chr $J1 $P1 | \
+java -jar $varscan copynumber - varScan.${sample}.${chr} --mpileup 1
+
+
+java -jar $varscan copyCaller varScan.${sample}.${chr}.copynumber --output-file varScan.${sample}.${chr}.copynumber.called
+
---
+++ Project/LianBaoFeng/varScan.p1n1.chr20.sh
@@ -0,0 +1,19 @@
+cd /netshare1/home1/people/hansun/Project/LianBaoFeng
+varscan=/netshare1/home1/people/hansun/MySoft/VarScan/VarScan.v2.3.3.jar
+hg19=/netshare1/home1/people/hansun/Project/LianBaoFeng/hg19.fa
+
+P1=/netshare1/home1/people/hansun/Project/LianBaoFeng/p1.rmdup.bam
+B1=/netshare1/home1/people/hansun/Project/LianBaoFeng/b1.rmdup.bam
+N1=/netshare1/home1/people/hansun/Project/LianBaoFeng/n1.rmdup.bam
+J1=/netshare1/home1/people/hansun/Project/LianBaoFeng/j1.rmdup.bam
+W1=/netshare1/home1/people/hansun/Project/LianBaoFeng/w1.rmdup.bam
+
+sample=P1N1
+chr=chr20
+
+samtools mpileup -q 1 -f $hg19 -r $chr $N1 $P1 | \
+java -jar $varscan copynumber - varScan.${sample}.${chr} --mpileup 1
+
+
+java -jar $varscan copyCaller varScan.${sample}.${chr}.copynumber --output-file varScan.${sample}.${chr}.copynumber.called
+
