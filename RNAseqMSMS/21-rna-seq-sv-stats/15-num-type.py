f809d51a520d8a196f4b294cd3a20cce7d0a0cde
hanice <sunahnice@gmail.com>
2013-08-29 06:00:27 +0800

    crontab.20130829

--- RNAseqMSMS/21-rna-seq-sv-stats/15-num-type.py
+++ RNAseqMSMS/21-rna-seq-sv-stats/15-num-type.py
@@ -1,5 +1,5 @@
 from PyPlot.PyPlotClass import *
 a=PyPlot('split-mapped-deletion-inversion-duplication-translocation.gene.pdf')
 #a.single_bar([84, 374, 1194, 3197],['4-types','3-types','2-types', '1-type'],yTitle='Number of Genes')
-a.single_bar([950, 81, 858, 170],['Deletion','Duplication','Inversion', 'Translocation'],yTitle='Number of Genes')
+a.single_bar([787, 54, 437, 29],['Deletion','Duplication','Inversion', 'Translocation'],yTitle='Number of Genes')
 
