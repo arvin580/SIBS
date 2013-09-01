d038e13e260d4bb2c2f05f244a556c576027a2f0
hanice <sunahnice@gmail.com>
2013-09-01 09:00:28 +0800

    crontab.20130901

--- RNAseqMSMS/23-rna-seq-virus-stats/HeLa-human-virus-final/7-mk-bed.py
+++ RNAseqMSMS/23-rna-seq-virus-stats/HeLa-human-virus-final/7-mk-bed.py
@@ -1,18 +1,17 @@
-def mk_bed(inF,desc):
-    I = 35
+POS = sorted([128241377,128231055,128241548,
+        128231213,128241370,128230632,128235913
+        ])
+
+def mk_bed(desc):
+    I = 20
     n = 0
-    inFile = open(inF)
-    ouFile = open(inF+'.bed','w')
+    ouFile = open('HeLa-chr8-Integration.bed','w')
     ouFile.write('browser position '+desc+'\n')
-    ouFile.write('track name="HPV18" description="HPV18 Integration" visibility=2 itemRgb="On"'+'\n')
-    for line in inFile:
+    ouFile.write('track name="HPV-18" description="HPV-18 Integration" visibility=2 itemRgb="On"'+'\n')
+    for pos in POS:
         n+=1
-        line = line.strip()
-        fields = line.split('\t')
-        ch = fields[0]
-        pos = int(fields[1])
-        ouFile.write(fields[0]+'\t'+str(pos)+'\t'+str(pos+I)+'\t'+'pos'+str(n)+'\t'+'0'+'\t'+'+'+'\t'+str(pos)+'\t'+str(pos+I)+'\t'+'255,0,0'+'\n')
-    inFile.close()
+        ch = 'chr8'
+        ouFile.write(ch+'\t'+str(pos)+'\t'+str(pos+I)+'\t'+'chr8:'+str(pos)+'\t'+'0'+'\t'+'+'+'\t'+str(pos)+'\t'+str(pos+I)+'\t'+'255,0,0'+'\n')
     ouFile.close()
 
-mk_bed('ERR0498-04-05.unmapped.unique.human-viruse-checked-chr8-128230000-128250000-human-chr-site-unique','chr8:128230000-128250000')
+mk_bed('chr8:128230000-128250000')
