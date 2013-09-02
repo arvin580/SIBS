1a15a398812f838e63e06d06a874e945b5996828
hanice <sunahnice@gmail.com>
2013-09-02 23:30:27 +0800

    crontab.20130902

--- RNAseqMSMS/23-rna-seq-virus-stats/HeLa-human-virus-final/HeLa-HPV-Integration-diagram.py
+++ RNAseqMSMS/23-rna-seq-virus-stats/HeLa-human-virus-final/HeLa-HPV-Integration-diagram.py
@@ -34,7 +34,7 @@ codes = [Path.MOVETO,
          Path.CLOSEPOLY,
          ]
 path = Path(verts, codes)
-patch = patches.PathPatch(path, facecolor='pink', lw=1)
+patch = patches.PathPatch(path, facecolor='orange', lw=1)
 ax.add_patch(patch)
 
 ### add integration point
@@ -51,26 +51,26 @@ for pos in POS:
 ### add gene annotation
 MYC = [(128748315,0.6),(128748315,0.66),(128753680,0.66),(128753680,0.6),(0,0)]
 path = Path(MYC, codes)
-patch = patches.PathPatch(path, facecolor='cyan', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((105+581)/2.0,-0.25,'E6',horizontalalignment='center',verticalalignment='center',fontsize=6)
 PCAT1 = [(128025399,0.6),(128025399,0.66),(128033259,0.66),(128033259,0.6),(0,0)]
 path = Path(PCAT1, codes)
-patch = patches.PathPatch(path, facecolor='cyan', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 
 CCAT1 = [(128219629,0.6),(128219629,0.66),(128231333,0.66),(128231333,0.6),(0,0)]
 path = Path(CCAT1, codes)
-patch = patches.PathPatch(path, facecolor='cyan', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 
 BC106081 = [(128240804,0.7),(128240804,0.76),(128241377,0.76),(128241377,0.7),(0,0)]
 path = Path(BC106081, codes)
-patch = patches.PathPatch(path, facecolor='cyan', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 
 ax.axes.get_yaxis().set_visible(False)
-#ax.axes.get_xaxis().set_visible(False)
+ax.axes.get_xaxis().set_visible(False)
 
 #################################################
 ################################################
@@ -116,51 +116,54 @@ for pos in POS:
 ## add gene annotation
 E6 = [(105,0.3),(105,0.36),(581,0.36),(581,0.3),(0,0)]
 path = Path(E6, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((105+581)/2.0,-0.25,'E6',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E7 = [(590,0.2),(590,0.26),(907,0.26),(907,0.2),(0,0)]
 path = Path(E7, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((590+907)/2.0,-0.45,'E7',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E1 = [(914,0.3),(914,0.36),(2887,0.36),(2887,0.3),(0,0)]
 path = Path(E1, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((914+2887)/2.0,-0.25,'E1',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E2 = [(2817,0.2),(2817,0.26),(3914,0.26),(3914,0.2),(0,0)]
 path = Path(E2, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((2817+3914)/2.0,-0.45,'E2',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E4 = [(3418,0.3),(3418,0.36),(3684,0.36),(3684,0.3),(0,0)]
 path = Path(E4, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((3418+3684)/2.0,-0.25,'E4',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 E5 = [(3936,0.3),(3936,0.36),(4157,0.36),(4157,0.3),(0,0)]
 path = Path(E5, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((3936+4157)/2.0,-0.25,'E5',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 L2 = [(4244,0.2),(4244,0.26),(5632,0.26),(5632,0.2),(0,0)]
 path = Path(L2, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
-ax.text((4244+5632)/2.0,-0.45,'L2',horizontalalignment='center',verticalalignment='center',fontsize=6)
+#ax.text((4244+5632)/2.0,-0.45,'L2',horizontalalignment='center',verticalalignment='center',fontsize=6)
 
 L1 = [(5430,0.3),(5430,0.36),(7136,0.36),(7136,0.3),(0,0)]
 path = Path(L1, codes)
-patch = patches.PathPatch(path, facecolor='green', lw=1)
+patch = patches.PathPatch(path, facecolor='magenta', lw=1)
 ax.add_patch(patch)
 #ax.text((5430+7136)/2.0,-0.25,'L1',horizontalalignment='center',verticalalignment='center',fontsize=6)
+
+ax.axes.get_yaxis().set_visible(False)
+ax.axes.get_xaxis().set_visible(False)
 
 
 
