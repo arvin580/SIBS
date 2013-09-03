8888f2ca5de6eac0718abc2a0283f388ae03526a
hanice <sunahnice@gmail.com>
2013-09-03 23:30:37 +0800

    crontab.20130903

---
+++ RNAseqMSMS/22-rna-seq-snv-stats/3-gene.py
@@ -0,0 +1,21 @@
+def gene(inF):
+    D = {}
+    inFile = open(inF)
+    ouFile = open(inF+'.gene','w')
+    for line in inFile:
+        fields = line.split('\t')
+        gene = fields[1]
+        x = gene.split('(')[0]
+        y = x.split(';')
+        for z in y:
+            m = z.split(',')
+            for n in m:
+                D[n] = 1
+    for x in D:
+        ouFile.write(x+'\n')
+
+    inFile.close()
+    ouFile.close()
+
+gene('sum_snv.exome_summary.overall.filter.nonsynonymous-splicing')
+gene('sum_snv.exome_summary.indel.overall.filter')
