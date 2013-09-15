5e64a4133e52e4a1e7cdbce82cc5299ac954996f
hanice <sunahnice@gmail.com>
2013-09-15 19:00:31 +0800

    crontab.20130915

---
+++ PredictNcRNA/2-predict.py
@@ -0,0 +1,27 @@
+inFile = open('Homo_sapiens.GRCh37.70.pep.abinitio.fa.fa')
+S1 = inFile.read()
+inFile.close()
+
+inFile = open('Homo_sapiens.GRCh37.70.pep.all.fa.fa')
+S2 = inFile.read()
+inFile.close()
+
+inFile = open('human_uniprot_sprot.fa.fa')
+S3 = inFile.read()
+inFile.close()
+
+inFile = open('ERR0498-04-05.fastq.pep')
+ouFile = open('ERR0498-04-05.fastq.pep.predict','w')
+
+while True:
+    line1 = inFile.readline().strip()
+    line2 = inFile.readline().strip()
+    if line1:
+        if (line2 in S1) and (line2 not in S2) and (line2 not in S3):
+            ouFile.write(line1+'\n')
+            ouFile.write(line2+'\n')
+    else:
+        break
+
+inFile.close()
+ouFile.close()
--- PredictNcRNA/six.sh
+++ PredictNcRNA/six.sh
@@ -1,2 +1,3 @@
 cd /netshare1/home1/people/hansun/PredictNcRNA 
-python six-frame-translation-reads.py ERR0498-04-05.fastq
+#python six-frame-translation-reads.py ERR0498-04-05.fastq
+python 2-predict.py
