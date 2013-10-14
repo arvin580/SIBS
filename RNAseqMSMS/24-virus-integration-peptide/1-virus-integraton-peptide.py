f1fa41da26375b771d9d06eba7c0bbb6298bbfa6
hanice <sunahnice@gmail.com>
2013-10-15 01:30:15 +0800

    crontab.20131015

--- RNAseqMSMS/24-virus-integration-peptide/1-virus-integraton-peptide.py
+++ RNAseqMSMS/24-virus-integration-peptide/1-virus-integraton-peptide.py
@@ -3,4 +3,9 @@ inFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked.num-more_than_
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
+    pos1 = fields[10]
+    pos2 = fields[11]
+    pos3 = fields[22]
+    pos3 = fields[23]
+
 inFile.close()
