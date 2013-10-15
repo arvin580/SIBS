822f672fd8b7e829ac2428becaee6329f689fb13
hanice <sunahnice@gmail.com>
2013-10-16 07:30:28 +0800

    crontab.20131016

--- RNAseqMSMS/24-virus-integration-peptide/2-KR.py
+++ RNAseqMSMS/24-virus-integration-peptide/2-KR.py
@@ -4,9 +4,31 @@ ouFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked.num-more_than_
 while True:
     line1 = inFile.readline().strip()
     line2 = inFile.readline().strip()
+    seq = line2
     if line1:
         fields = line1.split('\t')
-        point = int(fields[-1])
+        point = int(fields[-1]) - 1
+        flag1 = 0
+        flag2 = 0
+        pep1 = []
+        pep2 = []
+        for i in range(point-1,-1,-1):
+            if seq[i]!='K' and seq[i]!='R':
+                pep1.append(seq[i])
+            else:
+                flag1 = 1
+                break
+        pep1 = pep1[::-1]
+        for i in range(point,len(seq)):
+            if seq[i]!='K' and seq[i]!='R':
+                pep2.append(seq[i])
+            else:
+                flag2 = 1
+                break
+        pep2.append(seq[i])
+        if flag1 ==1 and flag2 ==1:
+            #ouFile.write(fields[0]+'\n')
+            ouFile.write(''.join(pep1+pep2)+'\n')
     else:
         break
 inFile.close()
