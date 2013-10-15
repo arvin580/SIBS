332576bafd996b9a97daa15db78ced44b5d7c0ab
hanice <sunahnice@gmail.com>
2013-10-16 01:00:28 +0800

    crontab.20131016

--- RNAseqMSMS/24-virus-integration-peptide/1-virus-integraton-peptide.py
+++ RNAseqMSMS/24-virus-integration-peptide/1-virus-integraton-peptide.py
@@ -1,11 +1,19 @@
 inFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked.num-more_than_two')
+ouFile = open('ERR0498-04-05.unmapped.unique.human-viruse-checked.num-more_than_two.fa','w')
 
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    pos1 = fields[10]
-    pos2 = fields[11]
-    pos3 = fields[22]
-    pos3 = fields[23]
+    
+    info = fields[0] + ':' + fields[1]
+    for i in range(10,len(fields),26):
+        pos1 = fields[i]
+        pos2 = fields[i+1]
+        pos3 = fields[i+12]
+        pos4 = fields[i+13]
+        seq = fields[i+18].split('|')[0]
+        seq_id = fields[i-6]
+        ouFile.write('>'+info+'\t'+seq_id+'\t'+'\t'.join([pos1,pos2,pos3,pos4])+'\n')
+        ouFile.write(seq+'\n')
 
 inFile.close()
