a965a1ad6fc4dd0cbe781067170f688e961dbda3
hanice <sunahnice@gmail.com>
2013-11-14 02:30:42 +0800

    crontab.20131114

--- NonSynonymous-SNV-Hetrozygous/RNA-Seq-CMP/2-cmp.py
+++ NonSynonymous-SNV-Hetrozygous/RNA-Seq-CMP/2-cmp.py
@@ -1,15 +1,14 @@
 D = {}
-
-inFile = open('HeLa-variant-Trypsin-LysC-GluC-evidence-unique-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_minus-spec-to_validation_not_predict-sequest')
+inFile = open('HeLa-variant-Trypsin-LysC-GluC-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_check-spec-to_validation_not_predict-pFind3')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    pep = fields[7]
+    pep = fields[9]
     D[pep] = 1
 inFile.close()
 
 inFile = open('Peptides-Identified-Second-unSpec')
-ouFile = open('Peptides-Identified-Second-unSpec-maxquant-sequest', 'w')
+ouFile = open('Peptides-Identified-Second-unSpec-xtandem-pfind', 'w')
 while True:
     line1 = inFile.readline().strip()
     line2 = inFile.readline().strip()
