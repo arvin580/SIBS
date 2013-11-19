156359de95d944107d17ed1fefb85b4224accc0a
hanice <sunahnice@gmail.com>
2013-11-20 07:30:31 +0800

    crontab.20131120

---
+++ NonSynonymous-SNV-Hetrozygous/2-peptide.py
@@ -0,0 +1,26 @@
+import os
+DIR = 'output-First'
+ouFile = open('Peptides-Identified-First2', 'w')
+ouFile2 = open('Peptides-Identified-First-Spec2', 'w')
+D = {}
+Fs = os.listdir(DIR)
+for F in Fs:
+    if F[-4:] == '.fdr':
+        inFile = open(DIR + os.sep + F)
+        for line in inFile:
+            line = line.strip()
+            fields = line.split('\t')
+            protein = fields[1]
+            peptide = fields[3]
+            if protein.find('REVERSE') == -1:
+                D.setdefault(peptide, [])
+                D[peptide].append(line)
+                ouFile2.write(fields[0] + '\n')
+        inFile.close()
+
+for k in D:
+    ouFile.write('>' + '+++'.join(set(D[k])) + '\n')
+    ouFile.write(k + '\n')
+
+ouFile.close()
+ouFile2.close()
