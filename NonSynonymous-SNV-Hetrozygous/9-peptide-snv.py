3f57b6d1fec8d8e0333de6d30be36d60f2b2ce16
hanice <sunahnice@gmail.com>
2013-11-20 04:00:31 +0800

    crontab.20131120

--- NonSynonymous-SNV-Hetrozygous/9-peptide-snv.py
+++ NonSynonymous-SNV-Hetrozygous/9-peptide-snv.py
@@ -7,12 +7,15 @@ while True:
     line1 = inFile.readline().strip()
     line2 = inFile.readline().strip()
     if line1:
-        s = re.search(r'\s(.*?)\sVARIATION', line1)
+        s = re.search(r'\s(\w+)\sVARIATION', line1)
         if s:
-            print(s.group(1))
+            pep=s.group(1)
+            D.setdefault(pep, 0)
+            D[pep] += 1
     else:
         break
-
-
 inFile.close()
+for k in D:
+    if D[k] == 1:
+        print(k)
 ouFile.close()
