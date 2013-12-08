baa222a41d654051e16b727e08343313db32d0ba
hanice <sunahnice@gmail.com>
2013-12-08 08:00:28 +0800

    crontab.20131208

--- Project/FLNA/blastn.sh
+++ Project/FLNA/blastn.sh
@@ -6,6 +6,7 @@
 #blastn  -db $db  -query $query -out $out -outfmt 6    -num_threads 6
 #####################################################################
 
+cd /netshare1/home1/people/hansun/Project/FLNA 
 humandb=/netshare1/home1/people/hansun/Data/GenomeSeq/Human/ucsc.hg19.fasta
 mousedb=/netshare1/home1/people/hansun/Data/GenomeSeq/Mouse/ucsc.mouse.fasta
 ratdb=/netshare1/home1/people/hansun/Data/GenomeSeq/Rat/ucsc.rat.fasta
@@ -23,16 +24,72 @@ zebrafishdb=/netshare1/home1/people/hansun/Data/GenomeSeq/Zebrafish/ucsc.zebrafi
 
 
 
-cd /netshare1/home1/people/hansun/Data/S3/blast10
+query=flna.fa
+out=$query.human.blasted
+blastn  -db $humandb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.mouse.blasted
+blastn  -db $mousedb  -query $query -out $out -outfmt 6   
 
 
-for ((i=44;i<45;i++))
-do 
-	query=lncrna_lncdb_human_genomic.$i
-	out=lncdb.human.human.out.$i
-	#blastn  -db $chimpanzeedb  -query $query -out $out -outfmt 6    
-	blastn  -db $humandb  -query $query -out $out -outfmt 6   
-done
+query=flna.fa
+out=$query.rat.blasted
+blastn  -db $ratdb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.chicken.blasted
+blastn  -db $chickendb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.chimpanzee.blasted
+blastn  -db $chimpanzeedb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.cow.blasted
+blastn  -db $cowdb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.dog.blasted
+blastn  -db $dogdb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.elephant.blasted
+blastn  -db $elephantdb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.fugu.blasted
+blastn  -db $fugudb  -query $query -out $out -outfmt 6   
+
+
+query=flna.fa
+out=$query.opossum.blasted
+blastn  -db $opossumdb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.platypus.blasted
+blastn  -db $platypusdb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.tetraodon.blasted
+blastn  -db $tetraodondb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.xenopus.blasted
+blastn  -db $xenopusdb  -query $query -out $out -outfmt 6   
+
+query=flna.fa
+out=$query.zebrafish.blasted
+blastn  -db $zebrafishdb  -query $query -out $out -outfmt 6   
+
+
+
+
+
+
+
+
+
 
 
 
