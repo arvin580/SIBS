45f7cde1db318a9a356315a5bf4cf05f8ae1b15b
hanice <sunahnice@gmail.com>
2014-03-10 19:30:46 +0800

    crontab.20140310

--- PredictedGene/AftTandem/3-filter.py
+++ PredictedGene/AftTandem/3-filter.py
@@ -1,2 +1,40 @@
-inFile = open()
+D = {}
+inFile = open('../Homo_sapiens.GRCh37.75.pep.all.fa.fa')
+while True:
+    line1 = inFile.readline().strip()
+    line2 = inFile.readline().strip()
+    if line1:
+        D[line1] = line2
+    else:
+        break
 inFile.close()
+
+inFile = open('../human_uniprot_sprot_2014_03_10.fa')
+while True:
+    line1 = inFile.readline().strip()
+    line2 = inFile.readline().strip()
+    if line1:
+        D[line1] = line2
+    else:
+        break
+inFile.close()
+
+inFile = open('HeLa-peptide-Predicted-pep')
+ouFile1 = open('HeLa-peptide-Predicted-pep-Known', 'w')
+ouFile2 = open('HeLa-peptide-Predicted-pep-New', 'w')
+
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    pep = fields[0]
+    flag = 0
+    for k in D:
+        if pep in D[k]:
+            ouFile1.write(line + '\t' + k + '\t' + D[k] + '\n')
+            flag = 1
+    if flag == 0:
+        ouFile2.write(line + '\n')
+inFile.close()
+
+ouFile1.close()
+ouFile2.close()
