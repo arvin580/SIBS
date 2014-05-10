47f117132737637deb7bdaa66a0f8af853a5ff5c
hanice <sunahnice@gmail.com>
2014-05-11 01:01:27 +0800

    crontab.20140511

--- RNAseqMSMS/26-MRM-Validated-Peptides/4-intersection.py
+++ RNAseqMSMS/26-MRM-Validated-Peptides/4-intersection.py
@@ -1,16 +1,18 @@
 D = {}
-inFile = open('HeLa-variant-Trypsin-LysC-GluC-evidence-unique-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_minus-spec-to_validation_not_predict-sequest')
+inFile = open('HeLa-Peptide-Variant-MaxQuant-XTandem')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
     D[fields[7]] = line
 inFile.close()
 
-inFile = open('HeLa-variant-Trypsin-LysC-GluC-peptide-snv-indel-predict-sv-virus-gene-miss_cleavage-known_new-normal_check-spec-to_validation_not_predict-pFind3-pFind-Trypsin')
-ouFile = open('HeLa-Peptide-Variant-MaxQuant-XTandem', 'w')
+inFile = open('HeLa-Variant-Peptide')
+ouFile = open('HeLa-Variant-Peptide-Validated2', 'w')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    if fields[9] in D:
-        ouFile.write(D[fields[9]] + '\n')
+    if fields[0] in D:
+        ouFile.write(D[fields[0]] + '\n')
+    else:
+        print(fields[0])
 inFile.close()
